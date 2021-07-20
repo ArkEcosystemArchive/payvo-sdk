d78c7afb80fc8c7c838bfa16a59bc32b0d09e200
faustbrian <faustbrian@users.noreply.github.com>
2021-07-20 01:09:04 +0000

    style: resolve style guide violations

--- packages/sdk/source/services/transaction.contract.ts
+++ packages/sdk/source/services/transaction.contract.ts
@@ -64,7 +64,7 @@ export interface MultiSignatureInput extends TransactionInput {
 		// Advanced
 		mandatoryKeys?: string[];
 		numberOfSignatures?: number;
-		optionalKeys?:  string[];
+		optionalKeys?: string[];
 	};
 }
 
