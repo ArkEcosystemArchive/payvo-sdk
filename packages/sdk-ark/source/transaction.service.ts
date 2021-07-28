3677a407db1d630c58af743c02b6b6b434645a40
Brian Faust <faustbrian@users.noreply.github.com>
2021-07-28 12:24:17 +0300

    fix(ark): derive address if multi-signature asset is available (#115)

--- packages/sdk-ark/source/transaction.service.ts
+++ packages/sdk-ark/source/transaction.service.ts
@@ -244,6 +244,13 @@ export class TransactionService extends Services.AbstractTransactionService {
 				senderPublicKey = (await this.publicKeyService.fromWIF(input.signatory.signingKey())).publicKey;
 			}
 
+			if (input.signatory.actsWithMultiSignature()) {
+				address = (await this.addressService.fromMultiSignature(
+					input.signatory.asset().min,
+					input.signatory.asset().publicKeys,
+				)).address;
+			}
+
 			if (senderPublicKey) {
 				transaction.senderPublicKey(senderPublicKey);
 			}
