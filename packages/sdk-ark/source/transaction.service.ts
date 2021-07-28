a3f4e119aa1818034c6a0c00372f53cc2c90e00f
faustbrian <faustbrian@users.noreply.github.com>
2021-07-28 14:16:19 +0000

    style: resolve style guide violations

--- packages/sdk-ark/source/transaction.service.ts
+++ packages/sdk-ark/source/transaction.service.ts
@@ -245,10 +245,12 @@ export class TransactionService extends Services.AbstractTransactionService {
 			}
 
 			if (input.signatory.actsWithMultiSignature()) {
-				address = (await this.addressService.fromMultiSignature(
-					input.signatory.asset().min,
-					input.signatory.asset().publicKeys,
-				)).address;
+				address = (
+					await this.addressService.fromMultiSignature(
+						input.signatory.asset().min,
+						input.signatory.asset().publicKeys,
+					)
+				).address;
 			}
 
 			if (senderPublicKey) {
