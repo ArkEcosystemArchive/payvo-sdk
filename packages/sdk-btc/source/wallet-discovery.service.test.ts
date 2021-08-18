7b18332792c4bd21d7f3a82d3ee38326c6f60fab
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-08-18 09:32:56 -0300

    test identity options

--- packages/sdk-btc/source/wallet-discovery.service.test.ts
+++ packages/sdk-btc/source/wallet-discovery.service.test.ts
@@ -20,7 +20,6 @@ beforeAll(async () => {
 
 describe("WalletDiscoveryService", () => {
 	it("should generate an output from a mnemonic", async () => {
-		console.log(identity.mnemonic);
 		const result = await subject.fromMnemonic(identity.mnemonic);
 
 		expect(result).toBeArrayOfSize(3);
@@ -39,5 +38,29 @@ describe("WalletDiscoveryService", () => {
 			path: "m/84'/1'/0'/0/0",
 			type: "bip84",
 		});
+	});
+	it("should generate an output from a mnemonic for specific paths for each addressing schema", async () => {
+		const result = await subject.fromMnemonic(identity.mnemonic, {
+			bip44: {
+				account: 1,
+				change: 0,
+				addressIndex: 3,
+			},
+			bip49: {
+				account: 1,
+				change: 1,
+				addressIndex: 2,
+			},
+			bip84: {
+				account: 1,
+				change: 0,
+				addressIndex: 4,
+			}
+		});
+
+		expect(result).toBeArrayOfSize(3);
+		expect(result[0]).toEqual({"address": "mgG73hJV966H8j4m3Ltbcikqh8j1mQXiWH", "path": "m/44'/1'/1'/0/3", "type": "bip44"});
+		expect(result[1]).toEqual({"address": "2MyfuuAKVRCZFa5RvqDZ3H9ckUjzRkSXq6R", "path": "m/49'/1'/1'/0/2", "type": "bip49"});
+		expect(result[2]).toEqual({"address": "tb1q26un8dvdan9kpvlyqn049jkhljhmckewkm9haz", "path": "m/84'/1'/1'/0/4", "type": "bip84"});
 	});
 });
