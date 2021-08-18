2ece89bd43b3a1f17fc5729137efc97623399a91
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-08-18 11:04:56 -0300

    tests for change addresses

--- packages/sdk-btc/source/wallet-discovery.service.test.ts
+++ packages/sdk-btc/source/wallet-discovery.service.test.ts
@@ -8,59 +8,214 @@ import { WalletDiscoveryService } from "./wallet-discovery.service";
 import { BindingType } from "./constants";
 import { AddressFactory } from "./address.factory";
 
-let subject: WalletDiscoveryService;
-
 beforeAll(async () => {
 	nock.disableNetConnect();
-
-	subject = createService(WalletDiscoveryService, "btc.testnet", (container) => {
-		container.singleton(BindingType.AddressFactory, AddressFactory);
-	});
 });
 
-describe("WalletDiscoveryService", () => {
+describe("testnet", () => {
+	const subject = createService(WalletDiscoveryService, "btc.testnet", (container) => {
+		container.singleton(BindingType.AddressFactory, AddressFactory);
+	});
+
 	it("should generate an output from a mnemonic", async () => {
 		const result = await subject.fromMnemonic(identity.mnemonic);
 
-		expect(result).toBeArrayOfSize(3);
-		expect(result[0]).toEqual({
-			address: "n2qGdjfjmFyvAXqbErrtXpfypXhtbNWruM",
-			path: "m/44'/1'/0'/0/0",
-			type: "bip44",
-		});
-		expect(result[1]).toEqual({
-			address: "2N5Hnn7HAizAwizSUXsMtoGnBNuXdMxDzBt",
-			path: "m/49'/1'/0'/0/0",
-			type: "bip49",
-		});
-		expect(result[2]).toEqual({
-			address: "tb1qdyxry6tza2sflfzlr8w6m65873thva724yjlmw",
-			path: "m/84'/1'/0'/0/0",
-			type: "bip84",
-		});
+		expect(result).toEqual([
+			{
+				address: "n2qGdjfjmFyvAXqbErrtXpfypXhtbNWruM",
+				path: "m/44'/1'/0'/0/0",
+				type: "bip44",
+			},
+			{
+				address: "2N5Hnn7HAizAwizSUXsMtoGnBNuXdMxDzBt",
+				path: "m/49'/1'/0'/0/0",
+				type: "bip49",
+			},
+			{
+				address: "tb1qdyxry6tza2sflfzlr8w6m65873thva724yjlmw",
+				path: "m/84'/1'/0'/0/0",
+				type: "bip84",
+			},
+		]);
 	});
+
 	it("should generate an output from a mnemonic for specific paths for each addressing schema", async () => {
 		const result = await subject.fromMnemonic(identity.mnemonic, {
 			bip44: {
-				account: 1,
+				account: 7,
 				change: 0,
 				addressIndex: 3,
 			},
 			bip49: {
-				account: 1,
+				account: 8,
+				change: 0,
+				addressIndex: 2,
+			},
+			bip84: {
+				account: 9,
+				change: 0,
+				addressIndex: 4,
+			},
+		});
+
+		expect(result).toEqual([
+			{
+				address: "mjS2VkYDn9ZxB4pVTgQGy5wYiPPwTnQHij",
+				path: "m/44'/1'/7'/0/3",
+				type: "bip44",
+			},
+			{
+				address: "2NFZAMmZ86NKR5Jpe79dBXdMSGjGtCvwKzX",
+				path: "m/49'/1'/8'/0/2",
+				type: "bip49",
+			},
+			{
+				address: "tb1qst6pxn2p9ltqqmuzn2j6yd4hnt07nkv36z42am",
+				path: "m/84'/1'/9'/0/4",
+				type: "bip84",
+			},
+		]);
+	});
+
+	it("should generate an output from a mnemonic for change chain", async () => {
+		const result = await subject.fromMnemonic(identity.mnemonic, {
+			bip44: {
+				account: 5,
+				change: 1,
+				addressIndex: 2,
+			},
+			bip49: {
+				account: 5,
 				change: 1,
 				addressIndex: 2,
 			},
 			bip84: {
-				account: 1,
-				change: 0,
-				addressIndex: 4,
-			}
+				account: 5,
+				change: 1,
+				addressIndex: 2,
+			},
 		});
 
-		expect(result).toBeArrayOfSize(3);
-		expect(result[0]).toEqual({"address": "mgG73hJV966H8j4m3Ltbcikqh8j1mQXiWH", "path": "m/44'/1'/1'/0/3", "type": "bip44"});
-		expect(result[1]).toEqual({"address": "2MyfuuAKVRCZFa5RvqDZ3H9ckUjzRkSXq6R", "path": "m/49'/1'/1'/0/2", "type": "bip49"});
-		expect(result[2]).toEqual({"address": "tb1q26un8dvdan9kpvlyqn049jkhljhmckewkm9haz", "path": "m/84'/1'/1'/0/4", "type": "bip84"});
+		expect(result).toEqual([
+			{
+				address: "mnaedh1YpvPsgybYyoyUjtnYaq91DjSXi4",
+				path: "m/44'/1'/5'/1/2",
+				type: "bip44",
+			},
+			{
+				address: "2MuDFXtW2mF9rUPbwPF5y8Eu9nfyYGbVe88",
+				path: "m/49'/1'/5'/1/2",
+				type: "bip49",
+			},
+			{
+				address: "tb1qw8fsvjcr63w06yl579ssgmugm0g9kkd0wjlxn3",
+				path: "m/84'/1'/5'/1/2",
+				type: "bip84",
+			},
+		]);
+	});
+});
+
+describe("livenet", () => {
+	const subject = createService(WalletDiscoveryService, "btc.livenet", (container) => {
+		container.singleton(BindingType.AddressFactory, AddressFactory);
+	});
+
+	it("should generate an output from a mnemonic", async () => {
+		const result = await subject.fromMnemonic(identity.mnemonic);
+
+		expect(result).toEqual([
+			{
+				address: "1PLDRLacEkAaaiWnfojVDb5hWpwXvKJrRa",
+				path: "m/44'/0'/0'/0/0",
+				type: "bip44",
+			},
+			{
+				address: "3GU5e9mPrLgPemhawVHHrDt6bjZZ6M9CPc",
+				path: "m/49'/0'/0'/0/0",
+				type: "bip49",
+			},
+			{
+				address: "bc1qpeeu3vjrm9dn2y42sl926374y5cvdhfn5k7kxm",
+				path: "m/84'/0'/0'/0/0",
+				type: "bip84",
+			},
+		]);
+	});
+
+	it("should generate an output from a mnemonic for specific paths for each addressing schema", async () => {
+		const result = await subject.fromMnemonic(identity.mnemonic, {
+			bip44: {
+				account: 7,
+				change: 0,
+				addressIndex: 3,
+			},
+			bip49: {
+				account: 8,
+				change: 0,
+				addressIndex: 2,
+			},
+			bip84: {
+				account: 9,
+				change: 0,
+				addressIndex: 4,
+			},
+		});
+
+		expect(result).toEqual([
+			{
+				address: "1DFirytnzyKwMBA2T8JNhjzwEoEimKTCKd",
+				path: "m/44'/0'/7'/0/3",
+				type: "bip44",
+			},
+			{
+				address: "38LF6P6FtA8vBgU4t1CmREHa2cCEiNQ8qt",
+				path: "m/49'/0'/8'/0/2",
+				type: "bip49",
+			},
+			{
+				address: "bc1q54m8puq4lwfmtcyv44x9ls2mztn6sgvde50vmh",
+				path: "m/84'/0'/9'/0/4",
+				type: "bip84",
+			},
+		]);
+	});
+
+	it("should generate an output from a mnemonic for change chain", async () => {
+		const result = await subject.fromMnemonic(identity.mnemonic, {
+			bip44: {
+				account: 5,
+				change: 1,
+				addressIndex: 2,
+			},
+			bip49: {
+				account: 5,
+				change: 1,
+				addressIndex: 2,
+			},
+			bip84: {
+				account: 5,
+				change: 1,
+				addressIndex: 2,
+			},
+		});
+
+		expect(result).toEqual([
+			{
+				address: "1PSddm78a9UnGWvL5xVCAcV7HddY16Vgjv",
+				path: "m/44'/0'/5'/1/2",
+				type: "bip44",
+			},
+			{
+				address: "3CkZ9ZButJgfwEY3cdKWmoxAGXFMUcSRjB",
+				path: "m/49'/0'/5'/1/2",
+				type: "bip49",
+			},
+			{
+				address: "bc1qw46dyhspu2tr7vsclyr7g9l99wwp3hyk5w8qqn",
+				path: "m/84'/0'/5'/1/2",
+				type: "bip84",
+			},
+		]);
 	});
 });
