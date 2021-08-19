d2669ad3b71bc1430cbd1e552b8bd5f76ef23481
Brian Faust <hello@basecode.sh>
2021-08-19 17:03:21 +0300

    wip

--- packages/sdk-ada/source/coin.services.ts
+++ packages/sdk-ada/source/coin.services.ts
@@ -1,5 +1,6 @@
 import { AddressService } from "./address.service";
 import { ClientService } from "./client.service";
+import { ExtendedPublicKeyService } from "./extended-public-key.service";
 import { KeyPairService } from "./key-pair.service";
 import { MessageService } from "./message.service";
 import { PrivateKeyService } from "./private-key.service";
@@ -9,6 +10,7 @@ import { TransactionService } from "./transaction.service";
 export const Services = {
 	AddressService,
 	ClientService,
+	ExtendedPublicKeyService,
 	KeyPairService,
 	MessageService,
 	PrivateKeyService,
