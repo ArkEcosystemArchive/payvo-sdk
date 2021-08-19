6ed158d8d4af7be748dcc040844dda1214fbe7c9
Brian Faust <hello@basecode.sh>
2021-08-19 16:50:41 +0300

    wip

--- packages/sdk-btc/source/extended-public-key.service.ts
+++ packages/sdk-btc/source/extended-public-key.service.ts
@@ -4,29 +4,29 @@ import { IoC, Services } from "@payvo/sdk";
 @IoC.injectable()
 export class ExtendedPublicKeyService extends Services.AbstractExtendedPublicKeyService {
 	public override async fromMnemonic(mnemonic: string, options?: Services.IdentityOptions): Promise<string> {
-		let bip32 = BIP32.fromMnemonic(mnemonic);
+		let accountKey = BIP32.fromMnemonic(mnemonic);
 
 		if (options?.bip44) {
-			bip32 = bip32
+			accountKey = accountKey
 				.deriveHardened(44)
 				.deriveHardened(this.configRepository.get("network.constants.slip44"))
 				.deriveHardened(options?.bip44?.account || 0);
 		}
 
 		if (options?.bip49) {
-			bip32 = bip32
+			accountKey = accountKey
 				.deriveHardened(49)
 				.deriveHardened(this.configRepository.get("network.constants.slip44"))
 				.deriveHardened(options?.bip49?.account || 0);
 		}
 
 		if (options?.bip84) {
-			bip32 = bip32
+			accountKey = accountKey
 				.deriveHardened(84)
 				.deriveHardened(this.configRepository.get("network.constants.slip44"))
 				.deriveHardened(options?.bip84?.account || 0);
 		}
 
-		return bip32.neutered().toBase58();
+		return accountKey.neutered().toBase58();
 	}
 }
