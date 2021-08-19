8fe73280e55d258264752763d1330fe68b977544
Brian Faust <hello@basecode.sh>
2021-08-19 06:40:24 +0300

    wip

--- packages/sdk/source/signatories/signatory.test.ts
+++ packages/sdk/source/signatories/signatory.test.ts
@@ -111,6 +111,41 @@ describe("MnemonicSignatory", () => {
 		}
 	`);
 	});
+
+	test("#options", () => {
+		const subject = new Signatory(
+			new MnemonicSignatory({
+				signingKey: "signingKey",
+				address: "address",
+				publicKey: "publicKey",
+				privateKey: "privateKey",
+				options: {
+					bip44: {
+						account: 0,
+					},
+				},
+			}),
+			{
+				publicKeys: [
+					"0271e4ffe50f2955fe32f9e05fb29a23f7dfcce77fa4c8a76328c7ab735033f851",
+					"023197268b110ca9c695f181d43a159ce380902ec549fe641e8bda047da0daf989",
+					"032b0c8dccc71dde04bfc1281d3a35428a48acf0b72be9a3914d4ebca1d5a73c32",
+					"0380c64e07942aee235387b4cbdc00923f7f486b4f5051bef806e0514e93222dc5",
+					"029e4dac4887b1b5d764b877559ad5171932f75e4fdefcb9ee3a96adb78d254bc4",
+					"034996d0a7b9788386b9d8d6ae86af0a0d676aee657c69b3e506648c69e39c15ea",
+				],
+				min: 4,
+			},
+		);
+
+		expect(subject.options()).toMatchInlineSnapshot(`
+		Object {
+		  "bip44": Object {
+		    "account": 0,
+		  },
+		}
+	`);
+	});
 });
 
 describe("ConfirmationMnemonicSignatory", () => {
@@ -520,6 +555,39 @@ describe("PrivateKeySignatory", () => {
 		    "029e4dac4887b1b5d764b877559ad5171932f75e4fdefcb9ee3a96adb78d254bc4",
 		    "034996d0a7b9788386b9d8d6ae86af0a0d676aee657c69b3e506648c69e39c15ea",
 		  ],
+		}
+	`);
+	});
+
+	test("#options", () => {
+		const subject = new Signatory(
+			new PrivateKeySignatory({
+				signingKey: "signingKey",
+				address: "address",
+				options: {
+					bip44: {
+						account: 0,
+					},
+				},
+			}),
+			{
+				publicKeys: [
+					"0271e4ffe50f2955fe32f9e05fb29a23f7dfcce77fa4c8a76328c7ab735033f851",
+					"023197268b110ca9c695f181d43a159ce380902ec549fe641e8bda047da0daf989",
+					"032b0c8dccc71dde04bfc1281d3a35428a48acf0b72be9a3914d4ebca1d5a73c32",
+					"0380c64e07942aee235387b4cbdc00923f7f486b4f5051bef806e0514e93222dc5",
+					"029e4dac4887b1b5d764b877559ad5171932f75e4fdefcb9ee3a96adb78d254bc4",
+					"034996d0a7b9788386b9d8d6ae86af0a0d676aee657c69b3e506648c69e39c15ea",
+				],
+				min: 4,
+			},
+		);
+
+		expect(subject.options()).toMatchInlineSnapshot(`
+		Object {
+		  "bip44": Object {
+		    "account": 0,
+		  },
 		}
 	`);
 	});
