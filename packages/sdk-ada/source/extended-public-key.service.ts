8e2897f75ef19270f84e9f89c55d2ee4685562fa
Brian Faust <hello@basecode.sh>
2021-08-19 17:02:59 +0300

    wip

---
+++ packages/sdk-ada/source/extended-public-key.service.ts
@@ -0,0 +1,10 @@
+import { IoC, Services } from "@payvo/sdk";
+
+import { deriveAccountKey, deriveRootKey } from "./shelley";
+
+@IoC.injectable()
+export class ExtendedPublicKeyService extends Services.AbstractExtendedPublicKeyService {
+	public override async fromMnemonic(mnemonic: string, options?: Services.IdentityOptions): Promise<string> {
+		return deriveAccountKey(deriveRootKey(mnemonic), options?.bip44?.account || 0).to_public().to_bech32();
+	}
+}
