a7383ef280ecffa8535ad620f30ebb90b2de9902
marianogoldman <marianogoldman@users.noreply.github.com>
2021-08-27 14:46:58 +0000

    style: resolve style guide violations

--- packages/sdk-btc/source/transaction.service.ts
+++ packages/sdk-btc/source/transaction.service.ts
@@ -29,11 +29,17 @@ export class TransactionService extends Services.AbstractTransactionService {
 			}
 
 			// 1. Derive the sender address (corresponding to first address index for the wallet)
-			const { address, type, path } = await this.addressService.fromMnemonic(input.signatory.signingKey(), input.signatory.options());
+			const { address, type, path } = await this.addressService.fromMnemonic(
+				input.signatory.signingKey(),
+				input.signatory.options(),
+			);
 			console.log(type, address, path);
 
 			// 2. Aggregate the unspent transactions
-			const xpub = await this.extendedPublicKeyService.fromMnemonic(input.signatory.signingKey(), input.signatory.options());
+			const xpub = await this.extendedPublicKeyService.fromMnemonic(
+				input.signatory.signingKey(),
+				input.signatory.options(),
+			);
 			const unspent: UnspentTransaction[] = await this.#figureOutUtxos({
 				type: "extendedPublicKey",
 				value: xpub,
@@ -56,18 +62,18 @@ export class TransactionService extends Services.AbstractTransactionService {
 					// non-segwit inputs now require passing the whole previous tx as Buffer
 					nonWitnessUtxo: Buffer.from(
 						"0200000001f9f34e95b9d5c8abcd20fc5bd4a825d1517be62f0f775e5f36da944d9" +
-						"452e550000000006b483045022100c86e9a111afc90f64b4904bd609e9eaed80d48" +
-						"ca17c162b1aca0a788ac3526f002207bb79b60d4fc6526329bf18a77135dc566020" +
-						"9e761da46e1c2f1152ec013215801210211755115eabf846720f5cb18f248666fec" +
-						"631e5e1e66009ce3710ceea5b1ad13ffffffff01" +
-						// value in satoshis (Int64LE) = 0x015f90 = 90000
-						"905f010000000000" +
-						// scriptPubkey length
-						"19" +
-						// scriptPubkey
-						"76a9148bbc95d2709c71607c60ee3f097c1217482f518d88ac" +
-						// locktime
-						"00000000",
+							"452e550000000006b483045022100c86e9a111afc90f64b4904bd609e9eaed80d48" +
+							"ca17c162b1aca0a788ac3526f002207bb79b60d4fc6526329bf18a77135dc566020" +
+							"9e761da46e1c2f1152ec013215801210211755115eabf846720f5cb18f248666fec" +
+							"631e5e1e66009ce3710ceea5b1ad13ffffffff01" +
+							// value in satoshis (Int64LE) = 0x015f90 = 90000
+							"905f010000000000" +
+							// scriptPubkey length
+							"19" +
+							// scriptPubkey
+							"76a9148bbc95d2709c71607c60ee3f097c1217482f518d88ac" +
+							// locktime
+							"00000000",
 						"hex",
 					),
 				});
