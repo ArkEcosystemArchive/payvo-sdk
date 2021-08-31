a19377da7d4f0ef0df23046baa63b2b6d81beac7
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-08-31 08:51:08 -0300

    Undo

--- packages/sdk-ark/source/client.service.ts
+++ packages/sdk-ark/source/client.service.ts
@@ -217,8 +217,7 @@ export class ClientService extends Services.AbstractClientService {
 			result.body = null;
 		} else {
 			// @ts-ignore
-			const identifiers: Services.WalletIdentifier[] | undefined =
-				body.identifiers as Services.WalletIdentifier[];
+			const identifiers: Services.WalletIdentifier[] | undefined = body.identifiers as Services.WalletIdentifier[];
 			if (identifiers) {
 				result.body.addresses = identifiers.map(({ value }) => value);
 
