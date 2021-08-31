495f85ee699bae8611d21689e1d5dbe7fabb455d
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-08-31 08:59:06 -0300

    fix tests

--- packages/sdk-btc/source/signed-transaction.dto.test.ts
+++ packages/sdk-btc/source/signed-transaction.dto.test.ts
@@ -11,8 +11,12 @@ beforeEach(() => {
 	subject = createService(SignedTransactionData);
 
 	subject.configure(
-		"3e3817fd0c35bc36674f3874c2953fa3e35877cbcdb44a08bdc6083dbd39d572",
+		"912ff5cac9d386fad9ad59a7661ed713990a8db12a801b34a3e8de0f27057371",
 		{
+			sender: "mv9pNZs3d65sjL68JueZDphWe3vHNmmSn6",
+			recipient: "tb1q705a7ak4ejlmfc5uq3afg2q45v4yw7kyv8jgsn",
+			amount: 100_000,
+			fee: 12_430,
 			timestamp: "1970-01-01T00:00:00.000Z",
 		},
 		"",
@@ -21,22 +25,22 @@ beforeEach(() => {
 
 describe("SignedTransactionData", () => {
 	test("#sender", () => {
-		expect(() => subject.sender()).toThrowError(Exceptions.NotImplemented);
+		expect(subject.sender()).toBe("mv9pNZs3d65sjL68JueZDphWe3vHNmmSn6");
 	});
 
 	test("#recipient", () => {
-		expect(() => subject.recipient()).toThrowError(Exceptions.NotImplemented);
+		expect(subject.recipient()).toBe("tb1q705a7ak4ejlmfc5uq3afg2q45v4yw7kyv8jgsn");
 	});
 
 	test("#amount", () => {
-		expect(() => subject.amount()).toThrowError(Exceptions.NotImplemented);
+		expect(subject.amount().toNumber()).toBe(100_000);
 	});
 
 	test("#fee", () => {
-		expect(() => subject.fee()).toThrowError(Exceptions.NotImplemented);
+		expect(subject.fee().toNumber()).toBe(12_430);
 	});
 
 	test("#timestamp", () => {
-		expect(() => subject.timestamp()).toThrowError(Exceptions.NotImplemented);
+		expect(subject.timestamp().toISOString()).toBe("1970-01-01T00:00:00.000Z");
 	});
 });
