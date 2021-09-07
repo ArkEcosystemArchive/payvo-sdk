860cc649f36cdad397773a5f5851de415d6b1195
Brian Faust <faustbrian@users.noreply.github.com>
2021-09-07 10:41:31 +0300

    feat(lsk): calculate fee from signed transaction data (#206)

--- packages/sdk-lsk/source/fee.service.test.ts
+++ packages/sdk-lsk/source/fee.service.test.ts
@@ -1,9 +1,10 @@
 import "jest-extended";
 
-import { IoC, Services } from "@payvo/sdk";
+import { IoC, Services, Signatories } from "@payvo/sdk";
 import nock from "nock";
 
 import { createService, require } from "../test/mocking";
+import { identity } from "../test/fixtures/identity";
 import { FeeService } from "./fee.service";
 import { DataTransferObjects } from "./coin.dtos";
 import { AddressService } from "./address.service";
@@ -15,6 +16,7 @@ import { MultiSignatureService } from "./multi-signature.service";
 import { TransactionSerializer } from "./transaction.serializer";
 import { BindingType } from "./coin.contract";
 import { AssetSerializer } from "./asset.serializer";
+import { TransactionService } from "./transaction.service";
 
 let subject: FeeService;
 
@@ -68,23 +70,59 @@ describe("FeeService", () => {
 		nock(/.+/)
 			.get("/api/v2/fees")
 			.reply(200, await require(`../test/fixtures/client/fees.json`))
+			.get("/api/v2/accounts?address=lskp4agpmjwgw549xdrhgdt6dfwqrpvohgbkhyt8p")
+			.reply(200, await require(`../test/fixtures/musig/lskp4agpmjwgw549xdrhgdt6dfwqrpvohgbkhyt8p.json`))
+			.get("/api/v2/accounts?publicKey=ac574896c846b59477a9115b952563938c48d0096b84846c0b634a621e1774ed")
+			.reply(200, await require(`../test/fixtures/musig/lskp4agpmjwgw549xdrhgdt6dfwqrpvohgbkhyt8p.json`))
+			.get("/api/v2/accounts?address=lskn2de9mo9z3g9jvbpj4yjn84vrvjzcn5c5mon7a")
+			.reply(200, await require(`../test/fixtures/musig/lskn2de9mo9z3g9jvbpj4yjn84vrvjzcn5c5mon7a.json`))
 			.persist();
 
-		const transaction = {
-			moduleID: 4,
-			assetID: 0,
-			senderPublicKey: "5e93fd5cfe306ea2c34d7082a6c79692cf2f5c6e07aa6f9b4a11b4917d33f16b",
-			nonce: "2",
-			fee: "314000",
-			asset: {
+		const wallet1 = {
+			signingKey: "foil broccoli rare pony man umbrella visual cram wing rotate fall never",
+			address: "lskp4agpmjwgw549xdrhgdt6dfwqrpvohgbkhyt8p",
+			publicKey: "ac574896c846b59477a9115b952563938c48d0096b84846c0b634a621e1774ed",
+		};
+
+		const wallet2 = {
+			signingKey: "penalty name learn right reason inherit peace mango guitar heart nature love",
+			address: "lskn2de9mo9z3g9jvbpj4yjn84vrvjzcn5c5mon7a",
+			publicKey: "5f7f98c50575a4a7e70a46ff35b72f4fe2a1ad3bc9a918b692d132d9c556bdf0",
+		};
+
+		const transaction = await (
+			await createService(TransactionService, "lsk.testnet", (container) => {
+				container.constant(IoC.BindingType.Container, container);
+				container.singleton(IoC.BindingType.AddressService, AddressService);
+				container.singleton(IoC.BindingType.ClientService, ClientService);
+				container.singleton(IoC.BindingType.FeeService, FeeService);
+				container.constant(IoC.BindingType.DataTransferObjects, DataTransferObjects);
+				container.singleton(
+					IoC.BindingType.DataTransferObjectService,
+					Services.AbstractDataTransferObjectService,
+				);
+				container.singleton(IoC.BindingType.KeyPairService, KeyPairService);
+				container.singleton(IoC.BindingType.LedgerService, LedgerService);
+				container.singleton(IoC.BindingType.PublicKeyService, PublicKeyService);
+				container.singleton(IoC.BindingType.MultiSignatureService, MultiSignatureService);
+				container.singleton(BindingType.AssetSerializer, AssetSerializer);
+				container.singleton(BindingType.TransactionSerializer, TransactionSerializer);
+			})
+		).multiSignature({
+			signatory: new Signatories.Signatory(
+				new Signatories.MnemonicSignatory({
+					signingKey: wallet1.signingKey,
+					address: wallet1.address,
+					publicKey: wallet1.publicKey,
+					privateKey: identity.privateKey,
+				}),
+			),
+			data: {
 				numberOfSignatures: 2,
-				mandatoryKeys: [
-					"5e93fd5cfe306ea2c34d7082a6c79692cf2f5c6e07aa6f9b4a11b4917d33f16b",
-					"c4fbe7dbc9cc52dd33b2b10cddd8d023c1ff30e1bcbb45fc813550540668295c",
-				],
+				mandatoryKeys: [wallet1.publicKey, wallet2.publicKey],
 				optionalKeys: [],
 			},
-		};
+		});
 
 		const slow = await subject.calculate(transaction, { priority: "slow" });
 		const average = await subject.calculate(transaction, { priority: "average" });
@@ -96,5 +134,81 @@ describe("FeeService", () => {
 		expect(average.toHuman()).toBe(0.00314);
 		expect(fast.toHuman()).toBeNumber();
 		expect(fast.toHuman()).toBe(0.00314);
+	});
+
+	test("#calculate with 5 participants", async () => {
+		nock(/.+/)
+			.get("/api/v2/fees")
+			.reply(200, await require(`../test/fixtures/client/fees.json`))
+			.get("/api/v2/accounts?address=lskp4agpmjwgw549xdrhgdt6dfwqrpvohgbkhyt8p")
+			.reply(200, await require(`../test/fixtures/musig/lskp4agpmjwgw549xdrhgdt6dfwqrpvohgbkhyt8p.json`))
+			.get("/api/v2/accounts?publicKey=ac574896c846b59477a9115b952563938c48d0096b84846c0b634a621e1774ed")
+			.reply(200, await require(`../test/fixtures/musig/lskp4agpmjwgw549xdrhgdt6dfwqrpvohgbkhyt8p.json`))
+			.get("/api/v2/accounts?address=lskn2de9mo9z3g9jvbpj4yjn84vrvjzcn5c5mon7a")
+			.reply(200, await require(`../test/fixtures/musig/lskn2de9mo9z3g9jvbpj4yjn84vrvjzcn5c5mon7a.json`))
+			.persist();
+
+		const wallet1 = {
+			signingKey: "foil broccoli rare pony man umbrella visual cram wing rotate fall never",
+			address: "lskp4agpmjwgw549xdrhgdt6dfwqrpvohgbkhyt8p",
+			publicKey: "ac574896c846b59477a9115b952563938c48d0096b84846c0b634a621e1774ed",
+		};
+
+		const wallet2 = {
+			signingKey: "penalty name learn right reason inherit peace mango guitar heart nature love",
+			address: "lskn2de9mo9z3g9jvbpj4yjn84vrvjzcn5c5mon7a",
+			publicKey: "5f7f98c50575a4a7e70a46ff35b72f4fe2a1ad3bc9a918b692d132d9c556bdf0",
+		};
+
+		const transaction = await (
+			await createService(TransactionService, "lsk.testnet", (container) => {
+				container.constant(IoC.BindingType.Container, container);
+				container.singleton(IoC.BindingType.AddressService, AddressService);
+				container.singleton(IoC.BindingType.ClientService, ClientService);
+				container.singleton(IoC.BindingType.FeeService, FeeService);
+				container.constant(IoC.BindingType.DataTransferObjects, DataTransferObjects);
+				container.singleton(
+					IoC.BindingType.DataTransferObjectService,
+					Services.AbstractDataTransferObjectService,
+				);
+				container.singleton(IoC.BindingType.KeyPairService, KeyPairService);
+				container.singleton(IoC.BindingType.LedgerService, LedgerService);
+				container.singleton(IoC.BindingType.PublicKeyService, PublicKeyService);
+				container.singleton(IoC.BindingType.MultiSignatureService, MultiSignatureService);
+				container.singleton(BindingType.AssetSerializer, AssetSerializer);
+				container.singleton(BindingType.TransactionSerializer, TransactionSerializer);
+			})
+		).multiSignature({
+			signatory: new Signatories.Signatory(
+				new Signatories.MnemonicSignatory({
+					signingKey: wallet1.signingKey,
+					address: wallet1.address,
+					publicKey: wallet1.publicKey,
+					privateKey: identity.privateKey,
+				}),
+			),
+			data: {
+				numberOfSignatures: 2,
+				mandatoryKeys: [
+					wallet1.publicKey,
+					wallet2.publicKey,
+					wallet2.publicKey,
+					wallet2.publicKey,
+					wallet2.publicKey,
+				],
+				optionalKeys: [],
+			},
+		});
+
+		const slow = await subject.calculate(transaction, { priority: "slow" });
+		const average = await subject.calculate(transaction, { priority: "average" });
+		const fast = await subject.calculate(transaction, { priority: "fast" });
+
+		expect(slow.toHuman()).toBeNumber();
+		expect(slow.toHuman()).toBe(0.00615);
+		expect(average.toHuman()).toBeNumber();
+		expect(average.toHuman()).toBe(0.00615);
+		expect(fast.toHuman()).toBeNumber();
+		expect(fast.toHuman()).toBe(0.00615);
 	});
 });
