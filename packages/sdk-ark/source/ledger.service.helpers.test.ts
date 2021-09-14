4e331c58520cef3f0e155b2f21d971c95a24c143
Ehsan <me@ehsanabtahi.com>
2021-09-14 13:04:16 +0430

    chore: add compareVersion

---
+++ packages/sdk-ark/source/ledger.service.helpers.test.ts
@@ -0,0 +1,20 @@
+import { compareVersion } from "./ledger.service.helpers";
+
+describe("compareVersion", () => {
+	it("should compare the current version with the minimum version", () => {
+		expect(compareVersion("81.0.1212.121","80.4.1121.121")).toBeTruthy();
+		expect(compareVersion("81.0.1212.121","80.4.9921.121")).toBeTruthy();
+		expect(compareVersion("80.0.1212.121","80.4.9921.121")).toBeFalsy();
+		expect(compareVersion("4.4.0","4.4.1")).toBeFalsy();
+		expect(compareVersion("5.24","5.2")).toBeTruthy();
+		expect(compareVersion("4.1","4.1.2")).toBeFalsy();
+		expect(compareVersion("4.1.2","4.1")).toBeTruthy();
+		expect(compareVersion("4.4.4.4","4.4.4.4.4")).toBeFalsy();
+		expect(compareVersion("4.4.4.4.4.4","4.4.4.4.4")).toBeTruthy();
+		expect(compareVersion("0","1")).toBeFalsy();
+		expect(compareVersion("1","1")).toBeTruthy();
+		expect(compareVersion("1","1.0.00000.0000")).toBeTruthy();
+		expect(compareVersion("","1")).toBeFalsy();
+		expect(compareVersion("10.0.1","10.1")).toBeFalsy();
+	});
+});
--- packages/sdk-ark/source/ledger.service.helpers.ts
+++ packages/sdk-ark/source/ledger.service.helpers.ts
@@ -9,3 +9,20 @@ export const formatLedgerDerivationPath = (scheme: LedgerDerivationScheme) =>
 	}`;
 
 export const createRange = (start: number, size: number) => Array.from({ length: size }, (_, i) => i + size * start);
+
+const normalizeVersion = (version: string) => {
+    return version.replace(/\./g," .").split(' ').map(versionPart => parseFloat(versionPart));
+};
+export const compareVersion = (currentVersion: string, minVersion: string) => {
+	let current = normalizeVersion(currentVersion);
+	let min = normalizeVersion(minVersion);
+
+	for(let i = 0; i < Math.max(current.length, min.length); i++) {
+		if((current[i] || 0) < (min[i] || 0)) {
+			return false;
+		} else if ((current[i] || 0) > (min[i] || 0)) {
+			return true;
+		}
+	}
+	return true;
+}
