d05f14e2fc3f06ecd9781d8f3c8afdecf3412421
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-10-04 09:56:40 -0300

    Functions to handle the musig addresses

--- packages/sdk-btc/source/address.domain.ts
+++ packages/sdk-btc/source/address.domain.ts
@@ -24,6 +24,53 @@ export const bip84 = (publicKey, network): string =>
 		network: network,
 	}).address!;
 
+export const rootToAccountKeys = (
+	rootKeys: bitcoin.BIP32Interface[],
+	derivationFn: (rootKey: bitcoin.BIP32Interface) => bitcoin.BIP32Interface,
+) => rootKeys.map(derivationFn);
+
+export const rootKeyToAccountKey = (rootKey: bitcoin.BIP32Interface, path: string): bitcoin.BIP32Interface =>
+	rootKey.derivePath(path);
+
+export const defaultLegacyMusigAccountKey = (rootKey: bitcoin.BIP32Interface): bitcoin.BIP32Interface =>
+	rootKeyToAccountKey(rootKey, "45'/0");
+
+export const defaultP2SHSegwitMusigAccountKey = (rootKey: bitcoin.BIP32Interface): bitcoin.BIP32Interface =>
+	rootKeyToAccountKey(rootKey, "48'/1'/0'/1'");
+
+export const defaultNativeSegwitMusigAccountKey = (rootKey: bitcoin.BIP32Interface): bitcoin.BIP32Interface =>
+	rootKeyToAccountKey(rootKey, "48'/1'/0'/2'");
+
+const sort = (a: Buffer, b: Buffer) => Buffer.compare(a, b);
+
+const createMusigPayment = (m: number, pubkeys: Buffer[], network: bitcoin.Network) =>
+	bitcoin.payments.p2ms({
+		m,
+		pubkeys: pubkeys.sort(sort),
+		network,
+	});
+
+export const createLegacyMusigAddress = (m: number, pubkeys: Buffer[], network: bitcoin.Network) =>
+	bitcoin.payments.p2sh({
+		redeem: createMusigPayment(m, pubkeys, network),
+		network,
+	});
+
+export const createP2SHSegwitMusigAddress = (m: number, pubkeys: Buffer[], network: bitcoin.Network) =>
+	bitcoin.payments.p2sh({
+		redeem: bitcoin.payments.p2wsh({
+			redeem: createMusigPayment(m, pubkeys, network),
+			network,
+		}),
+		network,
+	});
+
+export const createNativeSegwitMusigAddress = (m: number, pubkeys: Buffer[], network: bitcoin.Network) =>
+	bitcoin.payments.p2wsh({
+		redeem: createMusigPayment(m, pubkeys, network),
+		network,
+	});
+
 export const addressGenerator = function* (
 	bip: (publicKey, network) => string,
 	network: bitcoin.Network,
