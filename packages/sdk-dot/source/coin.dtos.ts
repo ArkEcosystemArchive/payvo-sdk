d2ea9a125b7ccb78557bae79634b712b8e3e9f19
Brian Faust <hello@basecode.sh>
2021-10-20 05:33:00 +0300

    wip

--- packages/sdk-dot/source/coin.dtos.ts
+++
@@ -1,7 +0,0 @@
-import { SignedTransactionData } from "./signed-transaction.dto";
-import { WalletData } from "./wallet.dto";
-
-export const DataTransferObjects = {
-	SignedTransactionData,
-	WalletData,
-};
