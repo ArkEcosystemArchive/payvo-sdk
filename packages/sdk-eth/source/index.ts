aeb155b6aa3978a95658b0f26dcf203e5087f083
Brian Faust <hello@basecode.sh>
2021-10-20 05:35:10 +0300

    wip

--- packages/sdk-eth/source/index.ts
+++ packages/sdk-eth/source/index.ts
@@ -1,9 +1,7 @@
 import { manifest } from "./manifest";
-import { ServiceProvider } from "./coin.provider";
 import { DataTransferObjects } from "./coin.dtos";
 
 export const ETH = {
 	dataTransferObjects: DataTransferObjects, // @TODO: consistent casing to avoid alias
 	manifest,
-	ServiceProvider,
 };
