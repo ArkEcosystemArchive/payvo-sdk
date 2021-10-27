2d829d9d94e008b9d204417877259f816314fe44
Nayiem <info@hodlercompany.com>
2021-10-27 13:19:40 +0200

    register XQR components in manifest

--- packages/sdk-ark/source/manifest.ts
+++ packages/sdk-ark/source/manifest.ts
@@ -2,6 +2,8 @@ import ArkDevnet from "./networks/ark.devnet";
 import ArkMainnet from "./networks/ark.mainnet";
 import BindMainnet from "./networks/bind.mainnet";
 import BindTestnet from "./networks/bind.testnet";
+import XqrMainnet from "./networks/xqr.mainnet";
+import XqrTestnet from "./networks/xqr.testnet";
 
 export const manifest = {
 	name: "ARK",
@@ -10,5 +12,7 @@ export const manifest = {
 		"ark.devnet": ArkDevnet,
 		"bind.mainnet": BindMainnet,
 		"bind.testnet": BindTestnet,
+		"xqr.mainnet": XqrMainnet,
+		"xqr.testnet": XqrTestnet,
 	},
 };
