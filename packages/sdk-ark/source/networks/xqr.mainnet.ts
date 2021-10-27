f966e7acc04340f47b2917cd6d119270afa6f92d
Nayiem Willems <37852059+NayiemWillems@users.noreply.github.com>
2021-10-27 13:20:12 +0200

    feat(ark): add Qredit network configurations (#302)

---
+++ packages/sdk-ark/source/networks/xqr.mainnet.ts
@@ -0,0 +1,61 @@
+import { Networks } from "@payvo/sdk";
+
+import { explorer, featureFlags, importMethods, transactions } from "./shared";
+
+const network: Networks.NetworkManifest = {
+	id: "xqr.mainnet",
+	type: "live",
+	name: "Mainnet",
+	coin: "Qredit",
+	currency: {
+		ticker: "XQR",
+		symbol: "Q",
+		decimals: 8,
+	},
+	constants: {
+		slip44: 111,
+	},
+	hosts: [
+		{
+			type: "full",
+			host: "https://qredit.cloud/api",
+		},
+		{
+			type: "explorer",
+			host: "https://explorer.sh/qredit",
+		},
+	],
+	governance: {
+		delegateCount: 51,
+		votesPerWallet: 1,
+		votesPerTransaction: 1,
+	},
+	transactions: {
+		...transactions,
+		fees: {
+			type: "dynamic",
+			ticker: "XQR",
+		},
+		multiPaymentRecipients: 128,
+	},
+	importMethods,
+	featureFlags: {
+		...featureFlags,
+		Transaction: [
+			"delegateRegistration",
+			"delegateResignation",
+			"estimateExpiration",
+			"multiPayment",
+			"secondSignature",
+			"transfer",
+			"vote",
+		],
+	},
+	explorer,
+	knownWallets: "https://raw.githubusercontent.com/qredit/common/master/mainnet/known-wallets-extended.json",
+	meta: {
+		fastDelegateSync: true,
+	},
+};
+
+export default network;
---
+++ packages/sdk-ark/source/networks/xqr.testnet.ts
@@ -0,0 +1,60 @@
+import { Networks } from "@payvo/sdk";
+
+import { explorer, featureFlags, importMethods, transactions } from "./shared";
+
+const network: Networks.NetworkManifest = {
+	id: "xqr.testnet",
+	type: "live",
+	name: "Testnet",
+	coin: "Qredit",
+	currency: {
+		ticker: "dXQR",
+		symbol: "dQ",
+		decimals: 8,
+	},
+	constants: {
+		slip44: 1,
+	},
+	hosts: [
+		{
+			type: "full",
+			host: "https://qredit.dev/api",
+		},
+		{
+			type: "explorer",
+			host: "https://explorer.sh/qredit-testnet",
+		},
+	],
+	governance: {
+		delegateCount: 51,
+		votesPerWallet: 1,
+		votesPerTransaction: 1,
+	},
+	transactions: {
+		...transactions,
+		fees: {
+			type: "dynamic",
+			ticker: "dXQR",
+		},
+		multiPaymentRecipients: 128,
+	},
+	importMethods,
+	featureFlags: {
+		...featureFlags,
+		Transaction: [
+			"delegateRegistration",
+			"delegateResignation",
+			"estimateExpiration",
+			"multiPayment",
+			"secondSignature",
+			"transfer",
+			"vote",
+		],
+	},
+	explorer,
+	meta: {
+		fastDelegateSync: true,
+	},
+};
+
+export default network;
