3a75d72f7125caaa0407eac9dbcaedb42ed89e37
Brian Faust <22145591+faustbrian@users.noreply.github.com>
2021-10-29 05:53:39 +0300

    chore: update `@payvo/cryptography` dependency (#306)

--- packages/sdk-ark/source/client.service.ts
+++ packages/sdk-ark/source/client.service.ts
@@ -66,11 +66,11 @@ export class ClientService extends Services.AbstractClientService {
 			available: hasVoted ? 0 : 1,
 			votes: hasVoted
 				? [
-					{
-						id: vote,
-						amount: 0,
-					},
-				]
+						{
+							id: vote,
+							amount: 0,
+						},
+				  ]
 				: [],
 		};
 	}
@@ -144,7 +144,7 @@ export class ClientService extends Services.AbstractClientService {
 		).json();
 	}
 
-	async #post(path: string, { body, searchParams }: { body; searchParams?}): Promise<Contracts.KeyValuePair> {
+	async #post(path: string, { body, searchParams }: { body; searchParams? }): Promise<Contracts.KeyValuePair> {
 		return (
 			await this.httpClient.post(
 				`${Helpers.randomHostFromConfig(this.configRepository)}/${path}`,
@@ -297,6 +297,8 @@ export class ClientService extends Services.AbstractClientService {
 	}
 
 	#isLegacy(): boolean {
-		return ["bind", "bpl", "xqr"].some((coin: string) => this.configRepository.get<string>("network.id").startsWith(coin));
+		return ["bind", "bpl", "xqr"].some((coin: string) =>
+			this.configRepository.get<string>("network.id").startsWith(coin),
+		);
 	}
 }
