58f1462a1c690a0bd1036d15438c8110ca6d651f
Brian Faust <hello@basecode.sh>
2021-11-01 06:59:35 +0200

    chore(lsk): remove secondSignature transaction type support

--- packages/sdk-lsk/source/networks/shared.ts
+++ packages/sdk-lsk/source/networks/shared.ts
@@ -2,7 +2,7 @@ import { Networks } from "@payvo/sdk";
 
 export const transactions: Networks.NetworkManifestTransactions = {
 	expirationType: "height",
-	types: ["delegateRegistration", "secondSignature", "transfer", "unlockToken", "vote"],
+	types: ["delegateRegistration", "transfer", "unlockToken", "vote"],
 	fees: {
 		type: "size",
 		ticker: "LSK",
