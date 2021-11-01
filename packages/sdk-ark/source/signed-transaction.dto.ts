fab30b8b84a55c192d0dc0d4e3fbfe7c4a3cdf42
goga-m <goga.mamoulasvili@gmail.com>
2021-11-01 10:22:32 +0200

    fix(ark): parse timestamp based on it's type in `SignedTransactionData#timestamp`

--- packages/sdk-ark/source/signed-transaction.dto.ts
+++ packages/sdk-ark/source/signed-transaction.dto.ts
@@ -36,6 +36,10 @@ export class SignedTransactionData
 	}
 
 	public override timestamp(): DateTime {
+		if (typeof this.signedData.timestamp === "number") {
+			return DateTime.fromUnix(this.signedData.timestamp);
+		}
+
 		if (this.signedData.timestamp) {
 			return DateTime.make(this.signedData.timestamp);
 		}
