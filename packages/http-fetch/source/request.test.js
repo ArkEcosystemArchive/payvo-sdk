8c60a232c09420b74397ece4766043e6767b78bc
Brian Faust <22145591+faustbrian@users.noreply.github.com>
2021-11-18 06:40:48 +0200

    chore(http-fetch): replace `jest` with `uvu` (#387)

--- packages/http-fetch/source/request.test.js
+++ packages/http-fetch/source/request.test.js
@@ -1,9 +1,9 @@
 import { Http } from "@payvo/sdk";
-import nock from "nock";
+import { assert, nock, test } from "@payvo/sdk-test";
 
 import { Request } from "./request";
 
-let subject: Request;
+let subject;
 
 test.before(() => {
 	nock.disableNetConnect();
@@ -11,124 +11,124 @@ test.before(() => {
 	subject = new Request();
 });
 
-describe("Request", () => {
-	test("should get with params", async () => {
-		const responseBody = {
-			args: { key: "value" },
-			origin: "87.95.132.111,10.100.91.201",
-			url: "http://httpbin.org/get",
-		};
+test("should get with params", async () => {
+	const responseBody = {
+		args: { key: "value" },
+		origin: "87.95.132.111,10.100.91.201",
+		url: "http://httpbin.org/get",
+	};
 
-		nock("http://httpbin.org/").get("/get").query(true).reply(200, responseBody);
+	nock("http://httpbin.org/").get("/get").query(true).reply(200, responseBody);
 
-		const response = await subject.get("http://httpbin.org/get", { key: "value" });
+	const response = await subject.get("http://httpbin.org/get", { key: "value" });
 
-		assert.is(response.json(), responseBody);
-	});
-
-	test("should get without params", async () => {
-		const responseBody = {
-			args: {},
-			origin: "87.95.132.111,10.100.91.201",
-			url: "http://httpbin.org/get",
-		};
-
-		nock("http://httpbin.org/").get("/get").reply(200, responseBody);
-
-		const response = await subject.get("http://httpbin.org/get");
-
-		assert.is(response.json(), responseBody);
-	});
-
-	test("should post with body", async () => {
-		const responseBody = {
-			args: {},
-			data: '{"key":"value"}',
-			files: {},
-			form: {},
-			json: {
-				key: "value",
-			},
-			origin: "87.95.132.111,10.100.91.201",
-			url: "http://httpbin.org/post",
-		};
-
-		nock("http://httpbin.org/").post("/post").reply(200, responseBody);
-
-		const response = await subject.post("http://httpbin.org/post", { key: "value" });
-
-		assert.is(response.json(), responseBody);
-	});
-
-	test("should post with headers", async () => {
-		const responseBody = {
-			args: {},
-			data: '{"key":"value"}',
-			files: {},
-			form: {},
-			headers: { Authorization: "Bearer TOKEN" },
-			json: {
-				key: "value",
-			},
-			origin: "87.95.132.111,10.100.91.201",
-			url: "http://httpbin.org/post",
-		};
-
-		nock("http://httpbin.org/").post("/post").reply(200, responseBody);
-
-		const response = await subject
-			.asJson()
-			.withHeaders({ Authorization: "Bearer TOKEN" })
-			.post("http://httpbin.org/post", { key: "value" });
-
-		assert.is(response.json(), responseBody);
-	});
-
-	test("should post with form_params", async () => {
-		const responseBody = {
-			args: {},
-			data: '{"key":"value"}',
-			files: {},
-			form: {
-				key: "value",
-			},
-			json: {},
-			origin: "87.95.132.111,10.100.91.201",
-			url: "http://httpbin.org/post",
-		};
-
-		nock("http://httpbin.org/").post("/post").reply(200, responseBody);
-
-		const response = await subject.asForm().post("http://httpbin.org/post", { key: "value" });
-
-		assert.is(response.json(), responseBody);
-	});
-
-	test("should post with octet", async () => {
-		const responseBody = {
-			args: {},
-			data: '{"key":"value"}',
-			files: {},
-			form: {
-				key: "value",
-			},
-			json: {},
-			origin: "87.95.132.111,10.100.91.201",
-			url: "http://httpbin.org/post",
-		};
-
-		nock("http://httpbin.org/").post("/post").reply(200, responseBody);
-
-		const response = await subject
-			.bodyFormat("octet")
-			.post("http://httpbin.org/post", Buffer.from(JSON.stringify({ key: "value" })));
-
-		assert.is(response.json(), responseBody);
-	});
-
-	test("should handle 404s", async () => {
-		nock("http://httpbin.org/").get("/get").reply(404);
-
-		await assert.is(subject.get("http://httpbin.org/get")).rejects.toThrow(Http.RequestException);
-	});
+	assert.equal(response.json(), responseBody);
 });
+
+test("should get without params", async () => {
+	const responseBody = {
+		args: {},
+		origin: "87.95.132.111,10.100.91.201",
+		url: "http://httpbin.org/get",
+	};
+
+	nock("http://httpbin.org/").get("/get").reply(200, responseBody);
+
+	const response = await subject.get("http://httpbin.org/get");
+
+	assert.equal(response.json(), responseBody);
+});
+
+test("should post with body", async () => {
+	const responseBody = {
+		args: {},
+		data: '{"key":"value"}',
+		files: {},
+		form: {},
+		json: {
+			key: "value",
+		},
+		origin: "87.95.132.111,10.100.91.201",
+		url: "http://httpbin.org/post",
+	};
+
+	nock("http://httpbin.org/").post("/post").reply(200, responseBody);
+
+	const response = await subject.post("http://httpbin.org/post", { key: "value" });
+
+	assert.equal(response.json(), responseBody);
+});
+
+test("should post with headers", async () => {
+	const responseBody = {
+		args: {},
+		data: '{"key":"value"}',
+		files: {},
+		form: {},
+		headers: { Authorization: "Bearer TOKEN" },
+		json: {
+			key: "value",
+		},
+		origin: "87.95.132.111,10.100.91.201",
+		url: "http://httpbin.org/post",
+	};
+
+	nock("http://httpbin.org/").post("/post").reply(200, responseBody);
+
+	const response = await subject
+		.asJson()
+		.withHeaders({ Authorization: "Bearer TOKEN" })
+		.post("http://httpbin.org/post", { key: "value" });
+
+	assert.equal(response.json(), responseBody);
+});
+
+test("should post with form_params", async () => {
+	const responseBody = {
+		args: {},
+		data: '{"key":"value"}',
+		files: {},
+		form: {
+			key: "value",
+		},
+		json: {},
+		origin: "87.95.132.111,10.100.91.201",
+		url: "http://httpbin.org/post",
+	};
+
+	nock("http://httpbin.org/").post("/post").reply(200, responseBody);
+
+	const response = await subject.asForm().post("http://httpbin.org/post", { key: "value" });
+
+	assert.equal(response.json(), responseBody);
+});
+
+test("should post with octet", async () => {
+	const responseBody = {
+		args: {},
+		data: '{"key":"value"}',
+		files: {},
+		form: {
+			key: "value",
+		},
+		json: {},
+		origin: "87.95.132.111,10.100.91.201",
+		url: "http://httpbin.org/post",
+	};
+
+	nock("http://httpbin.org/").post("/post").reply(200, responseBody);
+
+	const response = await subject
+		.bodyFormat("octet")
+		.post("http://httpbin.org/post", Buffer.from(JSON.stringify({ key: "value" })));
+
+	assert.equal(response.json(), responseBody);
+});
+
+test("should handle 404s", async () => {
+	nock("http://httpbin.org/").get("/get").reply(404);
+
+	await assert.rejects(() => subject.get("http://httpbin.org/get"), "HTTP request returned status code 404");
+});
+
+test.run();
