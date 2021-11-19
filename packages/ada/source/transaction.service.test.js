a2ef76020e6cf3179587c14fb065426bbf026d67
Brian Faust <hello@basecode.sh>
2021-11-19 05:02:33 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/ada/source/transaction.service.test.js
+++ packages/ada/source/transaction.service.test.js
@@ -1,5 +1,5 @@
-import { assert, describe, loader, test } from "@payvo/sdk-test";
-import { IoC, Services, Signatories, Test } from "@payvo/sdk";
+import { assert, loader, test } from "@payvo/sdk-test";
+import { IoC, Services, Signatories } from "@payvo/sdk";
 import nock from "nock";
 
 import { createService } from "../test/mocking";
@@ -36,39 +36,35 @@ test.before(async () => {
 	nock.disableNetConnect();
 });
 
-describe("TransactionService", () => {
-	describe("#transfer", () => {
-		test("is correct", async () => {
-			nock(/.+/)
-				.post("/")
-				.reply(200, loader.json(`test/fixtures/transaction/transactions-page-1.json`))
-				.post("/")
-				.reply(200, loader.json(`test/fixtures/transaction/transactions-page-2.json`))
-				.post("/")
-				.reply(200, loader.json(`test/fixtures/transaction/utxos.json`))
-				.post("/")
-				.reply(200, loader.json(`test/fixtures/transaction/expiration.json`));
+test("#transfer", async () => {
+	nock(/.+/)
+		.post("/")
+		.reply(200, loader.json(`test/fixtures/transaction/transactions-page-1.json`))
+		.post("/")
+		.reply(200, loader.json(`test/fixtures/transaction/transactions-page-2.json`))
+		.post("/")
+		.reply(200, loader.json(`test/fixtures/transaction/utxos.json`))
+		.post("/")
+		.reply(200, loader.json(`test/fixtures/transaction/expiration.json`));
 
-			const result = await subject.transfer({
-				signatory: new Signatories.Signatory(
-					new Signatories.MnemonicSignatory({
-						signingKey:
-							"excess behave track soul table wear ocean cash stay nature item turtle palm soccer lunch horror start stumble month panic right must lock dress",
-						address:
-							"aec30330deaecdd7503195a0d730256faef87027022b1bdda7ca0a61bca0a55e4d575af5a93bdf4905a3702fadedf451ea584791d233ade90965d608bac57304",
-						publicKey: "publicKey",
-						privateKey: "privateKey",
-					}),
-				),
-				data: {
-					amount: 1,
-					to: "addr_test1qpz03ezdyda8ag724zp3n5fqulay02dp7j9mweyeylcaapsxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknscw3xw7",
-				},
-			});
-
-			assert.is(result instanceof SignedTransactionData);
-			assert.is(result.id(), "e2e75b04c4b1dc4d4b3db14166fb02cb26f5b9ed3c49b1e1c8379a21502dc77c");
-			assert.is(result.amount().toString(), "1000000");
-		});
+	const result = await subject.transfer({
+		signatory: new Signatories.Signatory(
+			new Signatories.MnemonicSignatory({
+				signingKey:
+					"excess behave track soul table wear ocean cash stay nature item turtle palm soccer lunch horror start stumble month panic right must lock dress",
+				address:
+					"aec30330deaecdd7503195a0d730256faef87027022b1bdda7ca0a61bca0a55e4d575af5a93bdf4905a3702fadedf451ea584791d233ade90965d608bac57304",
+				publicKey: "publicKey",
+				privateKey: "privateKey",
+			}),
+		),
+		data: {
+			amount: 1,
+			to: "addr_test1qpz03ezdyda8ag724zp3n5fqulay02dp7j9mweyeylcaapsxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknscw3xw7",
+		},
 	});
+
+	assert.is(result instanceof SignedTransactionData);
+	assert.is(result.id(), "e2e75b04c4b1dc4d4b3db14166fb02cb26f5b9ed3c49b1e1c8379a21502dc77c");
+	assert.is(result.amount().toString(), "1000000");
 });
