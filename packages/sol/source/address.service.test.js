4d94ac2597cec1bda86a79b19284ddfdea276b0b
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-11-24 09:53:42 +0100

    test(sol): consistent use of `describe` (#483)

--- packages/sol/source/address.service.test.js
+++ packages/sol/source/address.service.test.js
@@ -1,37 +1,37 @@
-import { assert, test } from "@payvo/sdk-test";
+import { describe } from "@payvo/sdk-test";
 import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { AddressService } from "./address.service";
 
 let subject;
 
-test.before.each(async () => {
-	subject = await createService(AddressService);
-});
+describe("AddressService", ({ beforeEach, it, assert }) => {
+	beforeEach(async () => {
+		subject = await createService(AddressService);
+	});
 
-test("should generate an output from a mnemonic", async () => {
-	assert.equal(await subject.fromMnemonic(identity.mnemonic), {
-		type: "bip44",
-		address: identity.address,
+	it("should generate an output from a mnemonic", async () => {
+		assert.equal(await subject.fromMnemonic(identity.mnemonic), {
+			type: "bip44",
+			address: identity.address,
+		});
+	});
+
+	it("should fail to generate an output from a privateKey", async () => {
+		assert.equal(await subject.fromPrivateKey(identity.privateKey), {
+			type: "bip44",
+			address: identity.address,
+		});
+	});
+
+	it("should generate an output from a publicKey", async () => {
+		assert.equal(await subject.fromPublicKey(identity.publicKey), {
+			type: "bip44",
+			address: identity.address,
+		});
+	});
+
+	it("should validate an address", async () => {
+		assert.true(await subject.validate(identity.address));
 	});
 });
-
-test("should fail to generate an output from a privateKey", async () => {
-	assert.equal(await subject.fromPrivateKey(identity.privateKey), {
-		type: "bip44",
-		address: identity.address,
-	});
-});
-
-test("should generate an output from a publicKey", async () => {
-	assert.equal(await subject.fromPublicKey(identity.publicKey), {
-		type: "bip44",
-		address: identity.address,
-	});
-});
-
-test("should validate an address", async () => {
-	assert.true(await subject.validate(identity.address));
-});
-
-test.run();
--- packages/sol/source/key-pair.service.test.js
+++ packages/sol/source/key-pair.service.test.js
@@ -1,34 +1,34 @@
-import { assert, test } from "@payvo/sdk-test";
+import { describe } from "@payvo/sdk-test";
 import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { KeyPairService } from "./key-pair.service";
 
 let subject;
 
-test.before.each(async () => {
-	subject = await createService(KeyPairService);
-});
+describe("KeyPairService", async ({ beforeEach, assert, it }) => {
+	beforeEach(async () => {
+		subject = await createService(KeyPairService);
+	});
 
-test("should generate an output from a mnemonic", async () => {
-	const result = await subject.fromMnemonic(identity.mnemonic);
+	it("should generate an output from a mnemonic", async () => {
+		const result = await subject.fromMnemonic(identity.mnemonic);
 
-	assert.equal(result, {
-		privateKey: identity.privateKey,
-		publicKey: identity.publicKey,
+		assert.equal(result, {
+			privateKey: identity.privateKey,
+			publicKey: identity.publicKey,
+		});
+	});
+
+	it("should fail from an invalid mnemonic", async () => {
+		await assert.rejects(() => subject.fromMnemonic(identity.mnemonic.slice(0, 10)));
+	});
+
+	it("should generate an output from a privateKey", async () => {
+		const result = await subject.fromPrivateKey(identity.privateKey);
+
+		assert.equal(result, {
+			privateKey: identity.privateKey,
+			publicKey: identity.publicKey,
+		});
 	});
 });
-
-test("should fail from an invalid mnemonic", async () => {
-	await assert.rejects(() => subject.fromMnemonic(identity.mnemonic.slice(0, 10)));
-});
-
-test("should generate an output from a privateKey", async () => {
-	const result = await subject.fromPrivateKey(identity.privateKey);
-
-	assert.equal(result, {
-		privateKey: identity.privateKey,
-		publicKey: identity.publicKey,
-	});
-});
-
-test.run();
