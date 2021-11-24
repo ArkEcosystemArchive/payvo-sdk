db0918a3282f1ce57168fe9f2b84b9f8ac2bb471
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-11-24 13:07:05 +0100

    test(xrp): consistent use of `describe` (#491)

--- packages/xrp/source/address.service.test.js
+++ packages/xrp/source/address.service.test.js
@@ -1,29 +1,29 @@
-import { assert, test } from "@payvo/sdk-test";
+import { describe } from "@payvo/sdk-test";
 import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { AddressService } from "./address.service";
 
 let subject;
 
-test.before.each(async () => {
-	subject = await createService(AddressService);
+describe("AddressService", async ({ beforeEach, assert, it }) => {
+	beforeEach(async () => {
+		subject = await createService(AddressService);
+	});
+
+	it("should generate an output from a publicKey", async () => {
+		const result = await subject.fromPublicKey(identity.publicKey);
+
+		assert.equal(result, { type: "rfc6979", address: identity.address });
+	});
+
+	it("should generate an output from a secret", async () => {
+		const result = await subject.fromSecret(identity.mnemonic);
+
+		assert.equal(result, { type: "rfc6979", address: identity.address });
+	});
+
+	it("should validate an address", async () => {
+		assert.true(await subject.validate(identity.address));
+		assert.false(await subject.validate("invalid"));
+	});
 });
-
-test("should generate an output from a publicKey", async () => {
-	const result = await subject.fromPublicKey(identity.publicKey);
-
-	assert.equal(result, { type: "rfc6979", address: identity.address });
-});
-
-test("should generate an output from a secret", async () => {
-	const result = await subject.fromSecret(identity.mnemonic);
-
-	assert.equal(result, { type: "rfc6979", address: identity.address });
-});
-
-test("should validate an address", async () => {
-	assert.true(await subject.validate(identity.address));
-	assert.false(await subject.validate("invalid"));
-});
-
-test.run();
