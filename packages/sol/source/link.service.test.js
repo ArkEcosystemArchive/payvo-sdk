d95d872746fd007221ef1befef87f7647f85ec4c
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-11-24 09:44:38 +0100

    test(sol): consistent use of describe

--- packages/sol/source/link.service.test.js
+++ packages/sol/source/link.service.test.js
@@ -1,24 +1,24 @@
-import { assert, test } from "@payvo/sdk-test";
+import { describe } from "@payvo/sdk-test";
 import { Services } from "@payvo/sdk";
 
 import { createService } from "../test/mocking";
 
 let subject;
 
-test.before(async () => {
-	subject = await createService(Services.AbstractLinkService);
-});
+describe("LinkService", async ({ beforeAll, assert, it }) => {
+	beforeAll(async () => {
+		subject = await createService(Services.AbstractLinkService);
+	});
 
-test("should generate a link for a block", async () => {
-	assert.is(subject.block("id"), "https://explorer.solana.com/block/id?cluster=testnet");
-});
+	it("should generate a link for a block", async () => {
+		assert.is(subject.block("id"), "https://explorer.solana.com/block/id?cluster=testnet");
+	});
 
-test("should generate a link for a transaction", async () => {
-	assert.is(subject.transaction("id"), "https://explorer.solana.com/tx/id?cluster=testnet");
-});
+	it("should generate a link for a transaction", async () => {
+		assert.is(subject.transaction("id"), "https://explorer.solana.com/tx/id?cluster=testnet");
+	});
 
-test("should generate a link for a wallet", async () => {
-	assert.is(subject.wallet("id"), "https://explorer.solana.com/address/id?cluster=testnet");
+	it("should generate a link for a wallet", async () => {
+		assert.is(subject.wallet("id"), "https://explorer.solana.com/address/id?cluster=testnet");
+	});
 });
-
-test.run();
