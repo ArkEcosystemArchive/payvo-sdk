d887e074fa73e63a3fc4915cfd429733766a039e
Brian Faust <hello@basecode.sh>
2021-11-24 05:30:22 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/intl/source/money.test.js
+++ packages/intl/source/money.test.js
@@ -7,11 +7,11 @@ let subject;
 describe("Money", ({ assert, beforeEach, it }) => {
 	beforeEach(() => (subject = Money.make(5000, "EUR")));
 
-	it("should getAmount", () => {
+	it("should get the amount", () => {
 		assert.is(subject.getAmount(), 5000);
 	});
 
-	it("should setLocale", () => {
+	it("should set the locale", () => {
 		assert.is(subject.setLocale("de-DE").format(), "€50.00");
 	});
 
@@ -31,54 +31,54 @@ describe("Money", ({ assert, beforeEach, it }) => {
 		assert.is(subject.divide(10).getAmount(), 500);
 	});
 
-	it("should isEqualTo", () => {
+	it("should determine if the value is equal to another value", () => {
 		assert.true(subject.isEqualTo(Money.make(5000, "EUR")));
 		assert.false(subject.isEqualTo(Money.make(1000, "EUR")));
 	});
 
-	it("should isLessThan", () => {
+	it("should determine if the value is less than another value", () => {
 		assert.true(subject.isLessThan(Money.make(6000, "EUR")));
 		assert.false(subject.isLessThan(Money.make(5000, "EUR")));
 		assert.false(subject.isLessThan(Money.make(4000, "EUR")));
 	});
 
-	it("should isLessThanOrEqual", () => {
+	it("should determine if the value is less than or equal another value", () => {
 		assert.true(subject.isLessThanOrEqual(Money.make(5000, "EUR")));
 		assert.true(subject.isLessThanOrEqual(Money.make(6000, "EUR")));
 		assert.false(subject.isLessThanOrEqual(Money.make(4000, "EUR")));
 	});
 
-	it("should isGreaterThan", () => {
+	it("should determine if the value is greater than another value", () => {
 		assert.true(subject.isGreaterThan(Money.make(1000, "EUR")));
 		assert.true(subject.isGreaterThan(Money.make(1000, "EUR")));
 		assert.false(subject.isGreaterThan(Money.make(6000, "EUR")));
 	});
 
-	it("should isGreaterThanOrEqual", () => {
+	it("should determine if the value is greater than or equal another value", () => {
 		assert.true(subject.isGreaterThanOrEqual(Money.make(1000, "EUR")));
 		assert.true(subject.isGreaterThanOrEqual(Money.make(1000, "EUR")));
 		assert.false(subject.isGreaterThanOrEqual(Money.make(6000, "EUR")));
 	});
 
-	it("should isPositive", () => {
+	it("should determine if the value is positive", () => {
 		assert.true(Money.make(1, "EUR").isPositive());
 		assert.false(Money.make(-1, "EUR").isPositive());
 	});
 
-	it("should isNegative", () => {
+	it("should determine if the value is negative", () => {
 		assert.true(Money.make(-1, "EUR").isNegative());
 		assert.false(Money.make(1, "EUR").isNegative());
 	});
 
-	it("should getCurrency", () => {
+	it("should get the currency", () => {
 		assert.is(subject.getCurrency(), "EUR");
 	});
 
-	it("should format", () => {
+	it("should format it into a standardised string", () => {
 		assert.is(subject.format(), "€50.00");
 	});
 
-	it("should toUnit", () => {
+	it("should convert it to the unit (cents to euro)", () => {
 		assert.is(subject.toUnit(), 50);
 	});
 });
