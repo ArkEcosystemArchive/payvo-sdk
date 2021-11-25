8dc34fe52db863537f01a969ce2485e90e28e580
Brian Faust <hello@basecode.sh>
2021-11-25 11:20:31 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/test/source/hooks.ts
+++ packages/test/source/hooks.ts
@@ -1,5 +1,5 @@
 import { bgRed, bold, white } from "kleur";
-import { Context, test } from "uvu";
+import { Context } from "uvu";
 
 export const runHook = (callback: Function) => async (context: Context) => {
 	try {
@@ -8,9 +8,3 @@ export const runHook = (callback: Function) => async (context: Context) => {
 		console.log(bold(bgRed(white(error.stack))));
 	}
 };
-
-export const afterAll = async (callback_: Function) => test.after(runHook(callback_));
-export const afterEach = async (callback_: Function) => test.after.each(runHook(callback_));
-
-export const beforeAll = async (callback_: Function) => test.before(runHook(callback_));
-export const beforeEach = async (callback_: Function) => test.before.each(runHook(callback_));
