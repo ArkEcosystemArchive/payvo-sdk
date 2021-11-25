dbb5ae1d1cb7f269f908f259d277260efbc767e3
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2021-11-25 18:20:15 +0200

    test(xrp): consistent use of `context` (#514)

--- packages/xrp/source/transaction.dto.test.js
+++ packages/xrp/source/transaction.dto.test.js
@@ -6,24 +6,22 @@ import fixture from "../test/fixtures/client/transaction.json";
 import { createService } from "../test/mocking";
 import { ConfirmedTransactionData } from "./confirmed-transaction.dto";
 
-let subject;
-
 describe("ConfirmedTransactionData", async ({ beforeEach, it, assert }) => {
-	beforeEach(async () => {
-		subject = await createService(ConfirmedTransactionData);
-		subject.configure(fixture.result);
+	beforeEach(async (context) => {
+		context.subject = await createService(ConfirmedTransactionData);
+		context.subject.configure(fixture.result);
 	});
 
-	it("should succeed", async () => {
-		assert.instance(subject, ConfirmedTransactionData);
-		assert.is(subject.id(), "F4AB442A6D4CBB935D66E1DA7309A5FC71C7143ED4049053EC14E3875B0CF9BF");
-		assert.is(subject.type(), "transfer");
-		assert.instance(subject.timestamp(), DateTime);
-		assert.equal(subject.confirmations(), BigNumber.ZERO);
-		assert.is(subject.sender(), "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59");
-		assert.is(subject.recipient(), "r3PDtZSa5LiYp1Ysn1vMuMzB59RzV3W9QH");
-		assert.equal(subject.amount(), BigNumber.make(100000));
-		assert.equal(subject.fee(), BigNumber.make(1000));
-		assert.undefined(subject.memo());
+	it("should succeed", async (context) => {
+		assert.instance(context.subject, ConfirmedTransactionData);
+		assert.is(context.subject.id(), "F4AB442A6D4CBB935D66E1DA7309A5FC71C7143ED4049053EC14E3875B0CF9BF");
+		assert.is(context.subject.type(), "transfer");
+		assert.instance(context.subject.timestamp(), DateTime);
+		assert.equal(context.subject.confirmations(), BigNumber.ZERO);
+		assert.is(context.subject.sender(), "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59");
+		assert.is(context.subject.recipient(), "r3PDtZSa5LiYp1Ysn1vMuMzB59RzV3W9QH");
+		assert.equal(context.subject.amount(), BigNumber.make(100000));
+		assert.equal(context.subject.fee(), BigNumber.make(1000));
+		assert.undefined(context.subject.memo());
 	});
 });
