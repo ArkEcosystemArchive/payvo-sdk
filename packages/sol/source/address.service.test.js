644dcdae0d6ebbef6634ff991939807d75ad08af
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2021-11-25 18:25:54 +0200

    test(sol): consistent use of `context` (#516)

--- packages/sol/source/address.service.test.js
+++ packages/sol/source/address.service.test.js
@@ -3,35 +3,33 @@ import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { AddressService } from "./address.service";
 
-let subject;
-
 describe("AddressService", ({ beforeEach, it, assert }) => {
-	beforeEach(async () => {
-		subject = await createService(AddressService);
+	beforeEach(async (context) => {
+		context.subject = await createService(AddressService);
 	});
 
-	it("should generate an output from a mnemonic", async () => {
-		assert.equal(await subject.fromMnemonic(identity.mnemonic), {
+	it("should generate an output from a mnemonic", async (context) => {
+		assert.equal(await context.subject.fromMnemonic(identity.mnemonic), {
 			type: "bip44",
 			address: identity.address,
 		});
 	});
 
-	it("should fail to generate an output from a privateKey", async () => {
-		assert.equal(await subject.fromPrivateKey(identity.privateKey), {
+	it("should fail to generate an output from a privateKey", async (context) => {
+		assert.equal(await context.subject.fromPrivateKey(identity.privateKey), {
 			type: "bip44",
 			address: identity.address,
 		});
 	});
 
-	it("should generate an output from a publicKey", async () => {
-		assert.equal(await subject.fromPublicKey(identity.publicKey), {
+	it("should generate an output from a publicKey", async (context) => {
+		assert.equal(await context.subject.fromPublicKey(identity.publicKey), {
 			type: "bip44",
 			address: identity.address,
 		});
 	});
 
-	it("should validate an address", async () => {
-		assert.true(await subject.validate(identity.address));
+	it("should validate an address", async (context) => {
+		assert.true(await context.subject.validate(identity.address));
 	});
 });
