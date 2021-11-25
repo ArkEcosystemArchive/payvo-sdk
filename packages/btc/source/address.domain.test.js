ffa33feb8148497411a09f0adaa26983c3fa7396
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-11-25 11:48:09 +0100

    test(btc): consistent use of describe and context

--- packages/btc/source/address.domain.test.js
+++ packages/btc/source/address.domain.test.js
@@ -1,4 +1,4 @@
-import { assert, test } from "@payvo/sdk-test";
+import { describeWithContext } from "@payvo/sdk-test";
 import * as bitcoin from "bitcoinjs-lib";
 import { BIP32 } from "@payvo/sdk-cryptography";
 import {
@@ -13,111 +13,114 @@ import {
 import { musig } from "../test/fixtures/musig";
 import { convertBuffer } from "@payvo/sdk-helpers";
 
-const network = bitcoin.networks.testnet;
+describeWithContext(
+	"Address domain",
+	{
+		network: bitcoin.networks.testnet,
+		rootAccountKeys: musig.accounts.map((account) => BIP32.fromMnemonic(account.mnemonic, bitcoin.networks.testnet)),
+	},
+	async ({ it, assert }) => {
+		it("should derive account key for legacy multisig", async (context) => {
+			const accountKeys = rootToAccountKeys(context.rootAccountKeys, defaultLegacyMusigAccountKey);
 
-const rootAccountKeys = musig.accounts.map((account) => BIP32.fromMnemonic(account.mnemonic, network));
+			assert.equal(
+				accountKeys.map((accountKey) => convertBuffer(accountKey.publicKey)),
+				musig.legacyWallet.accountKeys,
+			);
+		});
 
-test("should derive account key for legacy multisig", async () => {
-	const accountKeys = rootToAccountKeys(rootAccountKeys, defaultLegacyMusigAccountKey);
+		it("should derive account key for p2sh-segwit multisig", async (context) => {
+			const accountKeys = rootToAccountKeys(context.rootAccountKeys, defaultP2SHSegwitMusigAccountKey);
 
-	assert.equal(
-		accountKeys.map((accountKey) => convertBuffer(accountKey.publicKey)),
-		musig.legacyWallet.accountKeys,
-	);
-});
+			assert.equal(
+				accountKeys.map((accountKey) => convertBuffer(accountKey.publicKey)),
+				musig.p2shSegwitWallet.accountKeys,
+			);
+		});
 
-test("should derive account key for p2sh-segwit multisig", async () => {
-	const accountKeys = rootToAccountKeys(rootAccountKeys, defaultP2SHSegwitMusigAccountKey);
+		it("should derive account key for native segwit multisig", async (context) => {
+			const accountKeys = rootToAccountKeys(context.rootAccountKeys, defaultNativeSegwitMusigAccountKey);
 
-	assert.equal(
-		accountKeys.map((accountKey) => convertBuffer(accountKey.publicKey)),
-		musig.p2shSegwitWallet.accountKeys,
-	);
-});
+			assert.equal(
+				accountKeys.map((accountKey) => convertBuffer(accountKey.publicKey)),
+				musig.nativeSegwitWallet.accountKeys,
+			);
+		});
 
-test("should derive account key for native segwit multisig", async () => {
-	const accountKeys = rootToAccountKeys(rootAccountKeys, defaultNativeSegwitMusigAccountKey);
+		it("should create a legacy multisig wallet like Electrum", async (context) => {
+			const accountKeys = rootToAccountKeys(context.rootAccountKeys, defaultLegacyMusigAccountKey);
 
-	assert.equal(
-		accountKeys.map((accountKey) => convertBuffer(accountKey.publicKey)),
-		musig.nativeSegwitWallet.accountKeys,
-	);
-});
+			musig.legacyWallet.spendAddresses.forEach((address, index) => {
+				assert.is(
+					legacyMusig(
+						2,
+						accountKeys.map((pk) => pk.derive(0).derive(index).publicKey),
+						context.network,
+					).address,
+					address,
+				);
+			});
+			musig.legacyWallet.changeAddresses.forEach((address, index) => {
+				assert.is(
+					legacyMusig(
+						2,
+						accountKeys.map((pk) => pk.derive(1).derive(index).publicKey),
+						context.network,
+					).address,
+					address,
+				);
+			});
+		});
 
-test("should create a legacy multisig wallet like Electrum", async () => {
-	const accountKeys = rootToAccountKeys(rootAccountKeys, defaultLegacyMusigAccountKey);
+		it("should create a p2sh-segwit (p2wsh-p2sh) multisig wallet like Electrum", async (context) => {
+			const accountKeys = rootToAccountKeys(context.rootAccountKeys, defaultP2SHSegwitMusigAccountKey);
 
-	musig.legacyWallet.spendAddresses.forEach((address, index) => {
-		assert.is(
-			legacyMusig(
-				2,
-				accountKeys.map((pk) => pk.derive(0).derive(index).publicKey),
-				network,
-			).address,
-			address,
-		);
-	});
-	musig.legacyWallet.changeAddresses.forEach((address, index) => {
-		assert.is(
-			legacyMusig(
-				2,
-				accountKeys.map((pk) => pk.derive(1).derive(index).publicKey),
-				network,
-			).address,
-			address,
-		);
-	});
-});
+			musig.p2shSegwitWallet.spendAddresses.forEach((address, index) => {
+				assert.is(
+					p2SHSegwitMusig(
+						2,
+						accountKeys.map((pk) => pk.derive(0).derive(index).publicKey),
+						context.network,
+					).address,
+					address,
+				);
+			});
+			musig.p2shSegwitWallet.changeAddresses.forEach((address, index) => {
+				assert.is(
+					p2SHSegwitMusig(
+						2,
+						accountKeys.map((pk) => pk.derive(1).derive(index).publicKey),
+						context.network,
+					).address,
+					address,
+				);
+			});
+		});
 
-test("should create a p2sh-segwit (p2wsh-p2sh) multisig wallet like Electrum", async () => {
-	const accountKeys = rootToAccountKeys(rootAccountKeys, defaultP2SHSegwitMusigAccountKey);
+		it("should create a native segwit (p2wsh) multisig wallet like Electrum", async (context) => {
+			const accountKeys = rootToAccountKeys(context.rootAccountKeys, defaultNativeSegwitMusigAccountKey);
 
-	musig.p2shSegwitWallet.spendAddresses.forEach((address, index) => {
-		assert.is(
-			p2SHSegwitMusig(
-				2,
-				accountKeys.map((pk) => pk.derive(0).derive(index).publicKey),
-				network,
-			).address,
-			address,
-		);
-	});
-	musig.p2shSegwitWallet.changeAddresses.forEach((address, index) => {
-		assert.is(
-			p2SHSegwitMusig(
-				2,
-				accountKeys.map((pk) => pk.derive(1).derive(index).publicKey),
-				network,
-			).address,
-			address,
-		);
-	});
-});
+			musig.nativeSegwitWallet.spendAddresses.forEach((address, index) => {
+				assert.is(
+					nativeSegwitMusig(
+						2,
+						accountKeys.map((pk) => pk.derive(0).derive(index).publicKey),
+						context.network,
+					).address,
+					address,
+				);
+			});
 
-test("should create a native segwit (p2wsh) multisig wallet like Electrum", async () => {
-	const accountKeys = rootToAccountKeys(rootAccountKeys, defaultNativeSegwitMusigAccountKey);
-
-	musig.nativeSegwitWallet.spendAddresses.forEach((address, index) => {
-		assert.is(
-			nativeSegwitMusig(
-				2,
-				accountKeys.map((pk) => pk.derive(0).derive(index).publicKey),
-				network,
-			).address,
-			address,
-		);
-	});
-
-	musig.nativeSegwitWallet.changeAddresses.forEach((address, index) => {
-		assert.is(
-			nativeSegwitMusig(
-				2,
-				accountKeys.map((pk) => pk.derive(1).derive(index).publicKey),
-				network,
-			).address,
-			address,
-		);
-	});
-});
-
-test.run();
+			musig.nativeSegwitWallet.changeAddresses.forEach((address, index) => {
+				assert.is(
+					nativeSegwitMusig(
+						2,
+						accountKeys.map((pk) => pk.derive(1).derive(index).publicKey),
+						context.network,
+					).address,
+					address,
+				);
+			});
+		});
+	},
+);
--- packages/btc/source/address.service.test.js
+++ packages/btc/source/address.service.test.js
@@ -1,4 +1,4 @@
-import { assert, describe, test } from "@payvo/sdk-test";
+import { describe } from "@payvo/sdk-test";
 
 import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
@@ -6,41 +6,33 @@ import { BindingType } from "./constants";
 import { AddressService } from "./address.service";
 import { AddressFactory } from "./address.factory";
 
-let subject;
-
-test.before(async () => {
-	subject = await createService(AddressService, undefined, async (container) => {
-		container.singleton(BindingType.AddressFactory, AddressFactory);
-	});
+const createMockService = () => createService(AddressService, undefined, async (container) => {
+	container.singleton(BindingType.AddressFactory, AddressFactory);
 });
 
 // These tests are based on the values from https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/test/integration/addresses.spec.ts
 
-describe("#fromMnemonic", ({ afterEach, beforeAll, test }) => {
-	beforeAll(async () => {
-		subject = await createService(AddressService, undefined, async (container) => {
-			container.singleton(BindingType.AddressFactory, AddressFactory);
-		});
-	});
+describe("#fromMnemonic", ({ beforeAll, assert, it }) => {
+	beforeAll(async (context) => (context.subject = await createMockService()));
 
-	test("should generate an output from a mnemonic (BIP44)", async () => {
-		const result = await subject.fromMnemonic(identity.mnemonic, { bip44: { account: 0 } });
+	it("should generate an output from a mnemonic (BIP44)", async (context) => {
+		const result = await context.subject.fromMnemonic(identity.mnemonic, { bip44: { account: 0 } });
 
 		assert.is(result.type, "bip44");
 		assert.is(result.address, "1PLDRLacEkAaaiWnfojVDb5hWpwXvKJrRa");
 		assert.is(result.path, "m/44'/0'/0'/0/0");
 	});
 
-	test("should generate an output from a mnemonic (BIP49)", async () => {
-		const result = await subject.fromMnemonic(identity.mnemonic, { bip49: { account: 0 } });
+	it("should generate an output from a mnemonic (BIP49)", async (context) => {
+		const result = await context.subject.fromMnemonic(identity.mnemonic, { bip49: { account: 0 } });
 
 		assert.is(result.type, "bip49");
 		assert.is(result.address, "3GU5e9mPrLgPemhawVHHrDt6bjZZ6M9CPc");
 		assert.is(result.path, "m/49'/0'/0'/0/0");
 	});
 
-	test("should generate an output from a mnemonic (BIP84)", async () => {
-		const result = await subject.fromMnemonic(identity.mnemonic, { bip84: { account: 0 } });
+	it("should generate an output from a mnemonic (BIP84)", async (context) => {
+		const result = await context.subject.fromMnemonic(identity.mnemonic, { bip84: { account: 0 } });
 
 		assert.is(result.type, "bip84");
 		assert.is(result.address, "bc1qpeeu3vjrm9dn2y42sl926374y5cvdhfn5k7kxm");
@@ -48,15 +40,11 @@ describe("#fromMnemonic", ({ afterEach, beforeAll, test }) => {
 	});
 });
 
-describe("#fromPublicKey", ({ afterEach, beforeAll, test }) => {
-	beforeAll(async () => {
-		subject = await createService(AddressService, undefined, async (container) => {
-			container.singleton(BindingType.AddressFactory, AddressFactory);
-		});
-	});
+describe("#fromPublicKey", ({ beforeAll, assert, it }) => {
+	beforeAll(async (context) => (context.subject = await createMockService()));
 
-	test("should import an address (via P2PKH)", async () => {
-		const result = await subject.fromPublicKey(
+	it("should import an address (via P2PKH)", async (context) => {
+		const result = await context.subject.fromPublicKey(
 			"0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
 			{ bip44: { account: 0 } },
 		);
@@ -65,16 +53,16 @@ describe("#fromPublicKey", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH");
 	});
 
-	test("should fail to generate address if ext pub key is not depth 3", async () => {
+	it("should fail to generate address if ext pub key is not depth 3", async (context) => {
 		await assert.rejects(() =>
-			subject.fromPublicKey(
+			context.subject.fromPublicKey(
 				"xpub6ENuDU6ouVBjsS46mpqzNzaJXs5iuNnhgKb9LWCgCwtK74fnATHwVJvsYYbH7bFUzZSh9PGA4Q9G5465WxHHRNys1hejSwbDZaw9ro5vDtD",
 			),
 		);
 	});
 
-	test("should generate a SegWit address (via P2SH)", async () => {
-		const result = await subject.fromPublicKey(
+	it("should generate a SegWit address (via P2SH)", async (context) => {
+		const result = await context.subject.fromPublicKey(
 			"0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
 			{ bip49: { account: 0 } },
 		);
@@ -83,8 +71,8 @@ describe("#fromPublicKey", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "3JvL6Ymt8MVWiCNHC7oWU6nLeHNJKLZGLN");
 	});
 
-	test("should generate a native SegWit address (via P2WPKH)", async () => {
-		const result = await subject.fromPublicKey(
+	it("should generate a native SegWit address (via P2WPKH)", async (context) => {
+		const result = await context.subject.fromPublicKey(
 			"0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
 			{ bip84: { account: 0 } },
 		);
@@ -93,8 +81,8 @@ describe("#fromPublicKey", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4");
 	});
 
-	test("should generate an address from an extended public key for livenet", async () => {
-		const result = await subject.fromPublicKey(
+	it("should generate an address from an extended public key for livenet", async (context) => {
+		const result = await context.subject.fromPublicKey(
 			"xpub6Bk8X5Y1FN7pSecqoqkHe8F8gNaqMVApCrmMxZnRvSw4JpgqeM5T83Ze6uD4XEMiCSwZiwysnny8uQj5F6XAPF9FNKYNHTMoAu97bDXNtRe",
 			{ bip44: { account: 0 } },
 		);
@@ -103,11 +91,11 @@ describe("#fromPublicKey", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "12KRAVpawWmzWNnv9WbqqKRHuhs7nFiQro");
 	});
 
-	test("should generate a Native SegWit address from an extended public key for tesnet", async () => {
-		subject = await createService(AddressService, "btc.testnet", async (container) => {
+	it("should generate a Native SegWit address from an extended public key for tesnet", async (context) => {
+		context.subject = await createService(AddressService, "btc.testnet", async (container) => {
 			container.singleton(BindingType.AddressFactory, AddressFactory);
 		});
-		const result = await subject.fromPublicKey(
+		const result = await context.subject.fromPublicKey(
 			"tpubDDVP3DS6MLMUwxsGKzPLjcwY38BmKpZT3USFmjWycwi441G3Mi6j7FhiHLpv2TzQLaAQ1iAun9Q1inpWB37pWEWPc5sZZNfLKoeRtR1ZANL",
 			{ bip84: { account: 0 } },
 		);
@@ -116,12 +104,12 @@ describe("#fromPublicKey", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "tb1q705a7ak4ejlmfc5uq3afg2q45v4yw7kyv8jgsn");
 	});
 
-	test("should generate a Native SegWit address from an extended public key for livenet", async () => {
-		subject = await createService(AddressService, "btc.livenet", async (container) => {
+	it("should generate a Native SegWit address from an extended public key for livenet", async (context) => {
+		context.subject = await createService(AddressService, "btc.livenet", async (container) => {
 			container.singleton(BindingType.AddressFactory, AddressFactory);
 		});
 
-		const result = await subject.fromPublicKey(
+		const result = await context.subject.fromPublicKey(
 			"xpub6Bk8X5Y1FN7pSecqoqkHe8F8gNaqMVApCrmMxZnRvSw4JpgqeM5T83Ze6uD4XEMiCSwZiwysnny8uQj5F6XAPF9FNKYNHTMoAu97bDXNtRe",
 			{ bip84: { account: 0 } },
 		);
@@ -130,12 +118,12 @@ describe("#fromPublicKey", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "bc1qpeeu3vjrm9dn2y42sl926374y5cvdhfn5k7kxm");
 	});
 
-	test("should generate a Native SegWit address from an extended public key for testnet", async () => {
-		subject = await createService(AddressService, "btc.testnet", async (container) => {
+	it("should generate a Native SegWit address from an extended public key for testnet", async (context) => {
+		context.subject = await createService(AddressService, "btc.testnet", async (container) => {
 			container.singleton(BindingType.AddressFactory, AddressFactory);
 		});
 
-		const result = await subject.fromPublicKey(
+		const result = await context.subject.fromPublicKey(
 			"tpubDC9zgMaiUXPoSRkpk8gvDuzwHobq6GUw5D1nzWdeBWrEXYZUxaQGDLwtXvVFsmvdNUVernGA2JWFbJsj4se5Vemx8WK6w9bzmxj4K36ivox",
 			{ bip84: { account: 0 } },
 		);
@@ -145,15 +133,11 @@ describe("#fromPublicKey", ({ afterEach, beforeAll, test }) => {
 	});
 });
 
-describe("#fromPrivateKey", ({ afterEach, beforeAll, test }) => {
-	beforeAll(async () => {
-		subject = await createService(AddressService, undefined, async (container) => {
-			container.singleton(BindingType.AddressFactory, AddressFactory);
-		});
-	});
+describe("#fromPrivateKey", ({ beforeAll, assert, it }) => {
+	beforeAll(async (context) => (context.subject = await createMockService()));
 
-	test("should import an address via WIF", async () => {
-		const result = await subject.fromPrivateKey(
+	it("should import an address via WIF", async (context) => {
+		const result = await context.subject.fromPrivateKey(
 			"0000000000000000000000000000000000000000000000000000000000000001",
 			{ bip44: { account: 0 } },
 		);
@@ -162,8 +146,8 @@ describe("#fromPrivateKey", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH");
 	});
 
-	test("should generate a SegWit address (via P2SH)", async () => {
-		const result = await subject.fromPrivateKey(
+	it("should generate a SegWit address (via P2SH)", async (context) => {
+		const result = await context.subject.fromPrivateKey(
 			"0000000000000000000000000000000000000000000000000000000000000001",
 			{ bip49: { account: 0 } },
 		);
@@ -172,8 +156,8 @@ describe("#fromPrivateKey", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "3JvL6Ymt8MVWiCNHC7oWU6nLeHNJKLZGLN");
 	});
 
-	test("should generate a SegWit address", async () => {
-		const result = await subject.fromPrivateKey(
+	it("should generate a SegWit address", async (context) => {
+		const result = await context.subject.fromPrivateKey(
 			"0000000000000000000000000000000000000000000000000000000000000001",
 			{ bip84: { account: 0 } },
 		);
@@ -183,15 +167,11 @@ describe("#fromPrivateKey", ({ afterEach, beforeAll, test }) => {
 	});
 });
 
-describe("#fromMultiSignature", ({ afterEach, beforeAll, test }) => {
-	beforeAll(async () => {
-		subject = await createService(AddressService, undefined, async (container) => {
-			container.singleton(BindingType.AddressFactory, AddressFactory);
-		});
-	});
+describe("#fromMultiSignature", ({ beforeAll, assert, it }) => {
+	beforeAll(async (context) => (context.subject = await createMockService()));
 
-	test("should generate a P2SH, pay-to-multisig (2-of-3) address", async () => {
-		const result = await subject.fromMultiSignature(
+	it("should generate a P2SH, pay-to-multisig (2-of-3) address", async (context) => {
+		const result = await context.subject.fromMultiSignature(
 			{
 				min: 2,
 				publicKeys: [
@@ -208,8 +188,8 @@ describe("#fromMultiSignature", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "36NUkt6FWUi3LAWBqWRdDmdTWbt91Yvfu7");
 	});
 
-	test("should generate a P2SH(P2WSH(...)), pay-to-multisig (2-of-2) address", async () => {
-		const result = await subject.fromMultiSignature(
+	it("should generate a P2SH(P2WSH(...)), pay-to-multisig (2-of-2) address", async (context) => {
+		const result = await context.subject.fromMultiSignature(
 			{
 				min: 2,
 				publicKeys: [
@@ -224,8 +204,8 @@ describe("#fromMultiSignature", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "3P4mrxQfmExfhxqjLnR2Ah4WES5EB1KBrN");
 	});
 
-	test("should generate a P2WSH (SegWit), pay-to-multisig (3-of-4) address", async () => {
-		const result = await subject.fromMultiSignature(
+	it("should generate a P2WSH (SegWit), pay-to-multisig (3-of-4) address", async (context) => {
+		const result = await context.subject.fromMultiSignature(
 			{
 				min: 3,
 				publicKeys: [
@@ -243,15 +223,11 @@ describe("#fromMultiSignature", ({ afterEach, beforeAll, test }) => {
 	});
 });
 
-describe("#fromWIF", ({ afterEach, beforeAll, test }) => {
-	beforeAll(async () => {
-		subject = await createService(AddressService, undefined, async (container) => {
-			container.singleton(BindingType.AddressFactory, AddressFactory);
-		});
-	});
+describe("#fromWIF", ({ beforeAll, it, assert }) => {
+	beforeAll(async (context) => (context.subject = await createMockService()));
 
-	test("should import an address via WIF", async () => {
-		const result = await subject.fromWIF("KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn", {
+	it("should import an address via WIF", async (context) => {
+		const result = await context.subject.fromWIF("KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn", {
 			bip44: { account: 0 },
 		});
 
@@ -259,8 +235,8 @@ describe("#fromWIF", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH");
 	});
 
-	test("should generate a SegWit address (via P2SH)", async () => {
-		const result = await subject.fromWIF("KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn", {
+	it("should generate a SegWit address (via P2SH)", async (context) => {
+		const result = await context.subject.fromWIF("KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn", {
 			bip49: { account: 0 },
 		});
 
@@ -268,8 +244,8 @@ describe("#fromWIF", ({ afterEach, beforeAll, test }) => {
 		assert.is(result.address, "3JvL6Ymt8MVWiCNHC7oWU6nLeHNJKLZGLN");
 	});
 
-	test("should generate a SegWit address", async () => {
-		const result = await subject.fromWIF("KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn", {
+	it("should generate a SegWit address", async (context) => {
+		const result = await context.subject.fromWIF("KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn", {
 			bip84: { account: 0 },
 		});
 
@@ -278,17 +254,19 @@ describe("#fromWIF", ({ afterEach, beforeAll, test }) => {
 	});
 });
 
-test("should validate an address", async () => {
-	assert.true(await subject.validate(identity.address));
-	assert.true(await subject.validate(identity.addressBIP44));
-	assert.true(await subject.validate(identity.addressBIP49));
-	assert.true(await subject.validate(identity.addressBIP84));
-	assert.false(await subject.validate("AdVSe37niA3uFUPgCgMUH2tMsHF4LpLoiX"));
-	assert.false(await subject.validate("ABC"));
-	assert.false(await subject.validate(""));
-	assert.false(await subject.validate(undefined));
-	assert.false(await subject.validate(null));
-	assert.false(await subject.validate({}));
-});
+describe('Address validation', async ({ it, assert, beforeAll }) => {
+	beforeAll(async (context) => (context.subject = await createMockService()));
 
-test.run();
+	it('should succeed', async (context) => {
+		assert.true(await context.subject.validate(identity.address));
+		assert.true(await context.subject.validate(identity.addressBIP44));
+		assert.true(await context.subject.validate(identity.addressBIP49));
+		assert.true(await context.subject.validate(identity.addressBIP84));
+		assert.false(await context.subject.validate("AdVSe37niA3uFUPgCgMUH2tMsHF4LpLoiX"));
+		assert.false(await context.subject.validate("ABC"));
+		assert.false(await context.subject.validate(""));
+		assert.false(await context.subject.validate(undefined));
+		assert.false(await context.subject.validate(null));
+		assert.false(await context.subject.validate({}));
+	});
+});
