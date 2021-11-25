d434f63505e323cda024d427612939d843e379b8
goga-m <goga.mamoulasvili@gmail.com>
2021-11-25 18:40:05 +0200

    wip

--- packages/neo/source/address.service.test.js
+++ packages/neo/source/address.service.test.js
@@ -3,15 +3,13 @@ import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { AddressService } from "./address.service";
 
-let subject;
-
 describe("AddressService", async ({ beforeEach, it, assert }) => {
-	beforeEach(async () => {
-		subject = await createService(AddressService);
+	beforeEach(async (context) => {
+		context.subject = await createService(AddressService);
 	});
 
-	it("should generate an output from a mnemonic", async () => {
-		const result = await subject.fromMnemonic(identity.mnemonic);
+	it("should generate an output from a mnemonic", async (context) => {
+		const result = await context.subject.fromMnemonic(identity.mnemonic);
 
 		assert.equal(result, {
 			address: "APPJtAkysCKBssD5EJzEpakntNk81nR7X2",
@@ -19,8 +17,8 @@ describe("AddressService", async ({ beforeEach, it, assert }) => {
 		});
 	});
 
-	it("should generate an output from a publicKey", async () => {
-		const result = await subject.fromPublicKey(identity.publicKey);
+	it("should generate an output from a publicKey", async (context) => {
+		const result = await context.subject.fromPublicKey(identity.publicKey);
 
 		assert.equal(result, {
 			address: "APPJtAkysCKBssD5EJzEpakntNk81nR7X2",
@@ -28,8 +26,8 @@ describe("AddressService", async ({ beforeEach, it, assert }) => {
 		});
 	});
 
-	it("should generate an output from a privateKey", async () => {
-		const result = await subject.fromPrivateKey(identity.privateKey);
+	it("should generate an output from a privateKey", async (context) => {
+		const result = await context.subject.fromPrivateKey(identity.privateKey);
 
 		assert.equal(result, {
 			address: "APPJtAkysCKBssD5EJzEpakntNk81nR7X2",
@@ -37,8 +35,8 @@ describe("AddressService", async ({ beforeEach, it, assert }) => {
 		});
 	});
 
-	it("should generate an output from a wif", async () => {
-		const result = await subject.fromWIF(identity.wif);
+	it("should generate an output from a wif", async (context) => {
+		const result = await context.subject.fromWIF(identity.wif);
 
 		assert.equal(result, {
 			address: "APPJtAkysCKBssD5EJzEpakntNk81nR7X2",
@@ -46,8 +44,8 @@ describe("AddressService", async ({ beforeEach, it, assert }) => {
 		});
 	});
 
-	it("should validate an address", async () => {
-		assert.true(await subject.validate("AdVSe37niA3uFUPgCgMUH2tMsHF4LpLoiX"));
-		assert.false(await subject.validate("ABC"));
+	it("should validate an address", async (context) => {
+		assert.true(await context.subject.validate("AdVSe37niA3uFUPgCgMUH2tMsHF4LpLoiX"));
+		assert.false(await context.subject.validate("ABC"));
 	});
 });
--- packages/neo/source/key-pair.service.test.js
+++ packages/neo/source/key-pair.service.test.js
@@ -3,29 +3,27 @@ import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { KeyPairService } from "./key-pair.service";
 
-let subject;
-
 describe("KeyPairService", async ({ beforeEach, it, assert }) => {
-	beforeEach(async () => {
-		subject = await createService(KeyPairService);
+	beforeEach(async (context) => {
+		context.subject = await createService(KeyPairService);
 	});
 
-	it("should generate an output from a mnemonic", async () => {
-		assert.equal(await subject.fromMnemonic(identity.mnemonic), {
+	it("should generate an output from a mnemonic", async (context) => {
+		assert.equal(await context.subject.fromMnemonic(identity.mnemonic), {
 			privateKey: identity.privateKey,
 			publicKey: identity.publicKey,
 		});
 	});
 
-	it("should generate an output from a privateKey", async () => {
-		assert.equal(await subject.fromPrivateKey(identity.privateKey), {
+	it("should generate an output from a privateKey", async (context) => {
+		assert.equal(await context.subject.fromPrivateKey(identity.privateKey), {
 			privateKey: identity.privateKey,
 			publicKey: identity.publicKey,
 		});
 	});
 
-	it("should generate an output from a wif", async () => {
-		assert.equal(await subject.fromWIF(identity.wif), {
+	it("should generate an output from a wif", async (context) => {
+		assert.equal(await context.subject.fromWIF(identity.wif), {
 			privateKey: identity.privateKey,
 			publicKey: identity.publicKey,
 		});
