3b011ec8f4744defb71018e25e6b2e93e3021f27
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-11-25 14:35:17 +0100

    wip

--- packages/btc/source/wif.service.test.js
+++ packages/btc/source/wif.service.test.js
@@ -1,18 +1,12 @@
-import { assert, test } from "@payvo/sdk-test";
+import { describe } from "@payvo/sdk-test";
 import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { WIFService } from "./wif.service";
 
-let subject;
+describe('WIFService', async ({ beforeEach, it, assert }) => {
+	beforeEach(async (context) => (context.subject = await createService(WIFService)));
 
-test.before.each(async () => {
-	subject = await createService(WIFService);
+	it("should generate an output from a mnemonic", async (context) => {
+		assert.equal(await context.subject.fromMnemonic(identity.mnemonic), { wif: identity.wif });
+	});
 });
-
-test("should generate an output from a mnemonic", async () => {
-	const result = await subject.fromMnemonic(identity.mnemonic);
-
-	assert.equal(result, { wif: identity.wif });
-});
-
-test.run();
