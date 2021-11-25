be3ea750def8f76be1523a4134a172dd8ed19191
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2021-11-25 18:20:22 +0200

    test(xlm): consistent use of `context` (#515)

--- packages/xlm/source/public-key.service.test.js
+++ packages/xlm/source/public-key.service.test.js
@@ -3,15 +3,13 @@ import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { PublicKeyService } from "./public-key.service";
 
-let subject;
-
 describe("PublicKeyService", async ({ beforeEach, it, assert }) => {
-	beforeEach(async () => {
-		subject = await createService(PublicKeyService);
+	beforeEach(async (context) => {
+		context.subject = await createService(PublicKeyService);
 	});
 
-	it("should generate an output from a mnemonic", async () => {
-		assert.equal(await subject.fromMnemonic(identity.mnemonic), {
+	it("should generate an output from a mnemonic", async (context) => {
+		assert.equal(await context.subject.fromMnemonic(identity.mnemonic), {
 			path: "m/44'/148'/0'",
 			publicKey: "GCGYSPQBSQCJKNDXDISBSXAM3THK7MACUVZGEMXF6XRZCPGAWCUGXVNC",
 		});
