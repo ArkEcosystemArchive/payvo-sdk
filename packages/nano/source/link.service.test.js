56cdc0d2b1ac852b6f5a9d2c0b0860d93392b508
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-11-25 01:47:37 +0100

    test(nano): consistent use of `describe` (#495)

--- packages/nano/source/link.service.test.js
+++ packages/nano/source/link.service.test.js
@@ -1,24 +1,24 @@
-import { assert, test } from "@payvo/sdk-test";
+import { describe } from "@payvo/sdk-test";
 import { Services } from "@payvo/sdk";
 
 import { createService } from "../test/mocking";
 
 let subject;
 
-test.before(async () => {
-	subject = await createService(Services.AbstractLinkService);
-});
+describe("LinkService", async ({ beforeAll, it, assert }) => {
+	beforeAll(async () => {
+		subject = await createService(Services.AbstractLinkService);
+	});
 
-test("should generate a link for a block", async () => {
-	assert.is(subject.block("id"), "https://nanocrawler.cc/explorer/block/id");
-});
+	it("should generate a link for a block", async () => {
+		assert.is(subject.block("id"), "https://nanocrawler.cc/explorer/block/id");
+	});
 
-test("should generate a link for a transaction", async () => {
-	assert.is(subject.transaction("id"), "https://nanocrawler.cc/explorer/block/id");
-});
+	it("should generate a link for a transaction", async () => {
+		assert.is(subject.transaction("id"), "https://nanocrawler.cc/explorer/block/id");
+	});
 
-test("should generate a link for a wallet", async () => {
-	assert.is(subject.wallet("id"), "https://nanocrawler.cc/explorer/account/id");
+	it("should generate a link for a wallet", async () => {
+		assert.is(subject.wallet("id"), "https://nanocrawler.cc/explorer/account/id");
+	});
 });
-
-test.run();
