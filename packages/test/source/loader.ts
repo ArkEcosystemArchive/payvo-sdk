c84afa471da452424952867d7e411ec172a6c050
Brian Faust <hello@basecode.sh>
2021-11-25 11:20:55 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/test/source/loader.ts
+++ packages/test/source/loader.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 
+// @TODO: turn this into a function that loads based on file extension
 export const loader = {
 	json: (path: string): Record<string, any> => JSON.parse(readFileSync(resolve(path)).toString()),
 };
