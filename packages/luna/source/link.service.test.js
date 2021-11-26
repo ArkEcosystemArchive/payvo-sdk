4c5a552a6eb505b48838f3eea6e38af82cc5d40f
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2021-11-26 02:10:36 +0200

    test(luna): consistent use of `context` (#524)

--- packages/luna/source/link.service.test.js
+++ packages/luna/source/link.service.test.js
@@ -3,22 +3,20 @@ import { Services } from "@payvo/sdk";
 
 import { createService } from "../test/mocking";
 
-let subject;
-
 describe("LinkService", async ({ beforeAll, it, assert }) => {
-	beforeAll(async () => {
-		subject = await createService(Services.AbstractLinkService);
+	beforeAll(async (context) => {
+		context.subject = await createService(Services.AbstractLinkService);
 	});
 
-	it("should generate a link for a block", async () => {
-		assert.is(subject.block("id"), "https://finder.terra.money/columbus-4/blocks/id");
+	it("should generate a link for a block", async (context) => {
+		assert.is(context.subject.block("id"), "https://finder.terra.money/columbus-4/blocks/id");
 	});
 
-	it("should generate a link for a transaction", async () => {
-		assert.is(subject.transaction("id"), "https://finder.terra.money/columbus-4/txs/id");
+	it("should generate a link for a transaction", async (context) => {
+		assert.is(context.subject.transaction("id"), "https://finder.terra.money/columbus-4/txs/id");
 	});
 
-	it("should generate a link for a wallet", async () => {
-		assert.is(subject.wallet("id"), "https://finder.terra.money/columbus-4/address/id");
+	it("should generate a link for a wallet", async (context) => {
+		assert.is(context.subject.wallet("id"), "https://finder.terra.money/columbus-4/address/id");
 	});
 });
