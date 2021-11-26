71c282b401fd8f36f9f01e4980b0eb50870db14d
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2021-11-26 15:43:33 +0200

    test(avax): consistent use of `context` (#542)

--- packages/avax/source/key-pair.service.test.js
+++ packages/avax/source/key-pair.service.test.js
@@ -3,23 +3,21 @@ import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { KeyPairService } from "./key-pair.service";
 
-let subject;
-
 describe("KeyPairService", async ({ assert, beforeEach, it }) => {
-	beforeEach(async () => {
-		subject = await createService(KeyPairService);
+	beforeEach(async (context) => {
+		context.subject = await createService(KeyPairService);
 	});
 
-	it("should generate an output from a mnemonic", async () => {
-		assert.equal(await subject.fromMnemonic(identity.mnemonic), {
+	it("should generate an output from a mnemonic", async (context) => {
+		assert.equal(await context.subject.fromMnemonic(identity.mnemonic), {
 			path: "m/44'/9000'/0'/0/0",
 			privateKey: "rC7DsPL1zKuPnwnqHSnShdXxeMReKWLBJgKcuJ1ZLUCUrzRni",
 			publicKey: "7qobgTQPiy3mH4tvjabDjapPVrh9Tnkb3tpn2yY37hsEyxaSjW",
 		});
 	});
 
-	it("should generate an output from a privateKey", async () => {
-		assert.equal(await subject.fromPrivateKey(identity.privateKey), {
+	it("should generate an output from a privateKey", async (context) => {
+		assert.equal(await context.subject.fromPrivateKey(identity.privateKey), {
 			publicKey: identity.publicKey,
 			privateKey: identity.privateKey,
 		});
