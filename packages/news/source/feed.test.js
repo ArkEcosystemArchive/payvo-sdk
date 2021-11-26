8101467d2581a7f53353c8d3ea13fcc9eacec6e2
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2021-11-26 02:20:00 +0200

    test(news): consistent use of `context` (#521)

--- packages/news/source/feed.test.js
+++ packages/news/source/feed.test.js
@@ -8,32 +8,30 @@ import { FeedService } from "./feed";
 
 const fixture = fs.readFileSync(resolve("test/fixtures/feed.xml")).toString();
 
-let subject;
-
 describe("FeedService", async ({ assert, beforeEach, it, nock }) => {
-	beforeEach(() => (subject = new FeedService()));
+	beforeEach((context) => (context.subject = new FeedService()));
 
-	it("should retrieve the feed and parse it", async () => {
+	it("should retrieve the feed and parse it", async (context) => {
 		nock.fake("https://blog.ark.io/").get("/feed").reply(200, fixture);
 
-		assert.object(await subject.parse("https://blog.ark.io/feed"));
+		assert.object(await context.subject.parse("https://blog.ark.io/feed"));
 	});
 
-	it("should throw an error when the request or parsing fails", async () => {
+	it("should throw an error when the request or parsing fails", async (context) => {
 		nock.fake("https://blog.ark.io/").get("/feed").reply(200, "malformed");
 
-		assert.rejects(() => subject.parse("https://blog.ark.io/feed"));
+		assert.rejects(() => context.subject.parse("https://blog.ark.io/feed"));
 	});
 
-	it("should retrieve the items of the feed", async () => {
+	it("should retrieve the items of the feed", async (context) => {
 		nock.fake("https://blog.ark.io/").get("/feed").reply(200, fixture);
 
-		assert.array(await subject.items("https://blog.ark.io/feed"));
+		assert.array(await context.subject.items("https://blog.ark.io/feed"));
 	});
 
-	it("should throw an error when the request or parsing fails", async () => {
+	it("should throw an error when the request or parsing fails", async (context) => {
 		nock.fake("https://blog.ark.io/").get("/feed").reply(200, "malformed");
 
-		assert.rejects(() => subject.items("https://blog.ark.io/feed"));
+		assert.rejects(() => context.subject.items("https://blog.ark.io/feed"));
 	});
 });
