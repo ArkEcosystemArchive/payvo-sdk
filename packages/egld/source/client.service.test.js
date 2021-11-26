a6049ed5f94bc782c5469fc96da5a59b41879cc6
goga-m <goga.mamoulasvili@gmail.com>
2021-11-26 15:03:23 +0200

    wip

--- packages/egld/source/client.service.test.js
+++ packages/egld/source/client.service.test.js
@@ -8,11 +8,9 @@ import { SignedTransactionData } from "./signed-transaction.dto";
 import { ConfirmedTransactionData } from "./confirmed-transaction.dto";
 import { WalletData } from "./wallet.dto";
 
-let subject;
-
 describe("ClientService", async ({ beforeAll, afterEach, assert, it }) => {
-	beforeAll(async () => {
-		subject = await createService(ClientService, undefined, (container) => {
+	beforeAll(async (context) => {
+		context.subject = await createService(ClientService, undefined, (container) => {
 			container.constant(IoC.BindingType.Container, container);
 			container.constant(IoC.BindingType.DataTransferObjects, {
 				SignedTransactionData,
@@ -27,34 +25,34 @@ describe("ClientService", async ({ beforeAll, afterEach, assert, it }) => {
 		nock.cleanAll();
 	});
 
-	it("#transaction should succeed", async () => {
+	it("#transaction should succeed", async (context) => {
 		nock.fake(/.+/)
 			.get("/v1.0/transaction/c2e6e2c75357b7d69d735d5ce7d7e9a77291477d0a11ba158b5cf39317398f66")
 			.reply(200, loader.json(`test/fixtures/client/transaction.json`));
 
-		const result = await subject.transaction("c2e6e2c75357b7d69d735d5ce7d7e9a77291477d0a11ba158b5cf39317398f66");
+		const result = await context.subject.transaction("c2e6e2c75357b7d69d735d5ce7d7e9a77291477d0a11ba158b5cf39317398f66");
 
 		assert.instance(result, ConfirmedTransactionData);
 	});
 
-	it("#transactions should succeed", async () => {
+	it("#transactions should succeed", async (context) => {
 		nock.fake(/.+/)
 			.get("/v1.0/address/erd17uy64y9zw8zd4xz5d2deqmxfxkk3zfuj0jh24k0s5jqhet3pz0esng60j7/transactions")
 			.reply(200, loader.json(`test/fixtures/client/transactions.json`));
 
-		const result = await subject.transactions({
+		const result = await context.subject.transactions({
 			identifiers: [{ type: "address", value: "erd17uy64y9zw8zd4xz5d2deqmxfxkk3zfuj0jh24k0s5jqhet3pz0esng60j7" }],
 		});
 
 		assert.instance(result.items()[0], ConfirmedTransactionData);
 	});
 
-	it("#wallet should succeed", async () => {
+	it("#wallet should succeed", async (context) => {
 		nock.fake(/.+/)
 			.get("/v1.0/address/erd17uy64y9zw8zd4xz5d2deqmxfxkk3zfuj0jh24k0s5jqhet3pz0esng60j7")
 			.reply(200, loader.json(`test/fixtures/client/wallet.json`));
 
-		const result = await subject.wallet({
+		const result = await context.subject.wallet({
 			type: "address",
 			value: "erd17uy64y9zw8zd4xz5d2deqmxfxkk3zfuj0jh24k0s5jqhet3pz0esng60j7",
 		});
--- packages/egld/source/link.service.test.js
+++ packages/egld/source/link.service.test.js
@@ -3,22 +3,20 @@ import { Services } from "@payvo/sdk";
 
 import { createService } from "../test/mocking";
 
-let subject;
-
 describe("LinkService", async ({ beforeAll, assert, it }) => {
-	beforeAll(async () => {
-		subject = await createService(Services.AbstractLinkService);
+	beforeAll(async (context) => {
+		context.subject = await createService(Services.AbstractLinkService);
 	});
 
-	it("should generate a link for a block", async () => {
-		assert.is(subject.block("id"), "https://testnet-explorer.elrond.com/miniblocks/id");
+	it("should generate a link for a block", async (context) => {
+		assert.is(context.subject.block("id"), "https://testnet-explorer.elrond.com/miniblocks/id");
 	});
 
-	it("should generate a link for a transaction", async () => {
-		assert.is(subject.transaction("id"), "https://testnet-explorer.elrond.com/transactions/id");
+	it("should generate a link for a transaction", async (context) => {
+		assert.is(context.subject.transaction("id"), "https://testnet-explorer.elrond.com/transactions/id");
 	});
 
-	it("should generate a link for a wallet", async () => {
-		assert.is(subject.wallet("id"), "https://testnet-explorer.elrond.com/accounts/id");
+	it("should generate a link for a wallet", async (context) => {
+		assert.is(context.subject.wallet("id"), "https://testnet-explorer.elrond.com/accounts/id");
 	});
 });
