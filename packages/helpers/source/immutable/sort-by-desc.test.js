cce1bfa6cad35716ad1524bf676511226009849a
goga-m <goga.mamoulasvili@gmail.com>
2021-11-26 14:53:32 +0200

    wip

--- packages/helpers/source/immutable/sort-by-desc.test.js
+++ packages/helpers/source/immutable/sort-by-desc.test.js
@@ -1,39 +1,41 @@
-import { describe } from "@payvo/sdk-test";
+import { describeWithContext } from "@payvo/sdk-test";
 
 import { sortByDesc } from "./sort-by-desc";
 
-const dummies = [
-	{ age: 18, name: "Andrew" },
-	{ age: 18, name: "Bob" },
-	{ age: 30, name: "John" },
-	{ age: 40, name: "Jane" },
-];
+describeWithContext(
+	"sortByDesc",
+	() => ({ dummies: [
+		{ age: 18, name: "Andrew" },
+		{ age: 18, name: "Bob" },
+		{ age: 30, name: "John" },
+		{ age: 40, name: "Jane" },
+	]}),
+	({ assert, it }) => {
+		it("should sort records without iteratees", (context) => {
+			assert.equal(sortByDesc(context.dummies), [
+				{ age: 40, name: "Jane" },
+				{ age: 30, name: "John" },
+				{ age: 18, name: "Bob" },
+				{ age: 18, name: "Andrew" },
+			]);
+		});
 
-describe("sortByDesc", async ({ assert, it }) => {
-	it("should sort records without iteratees", () => {
-		assert.equal(sortByDesc(dummies), [
-			{ age: 40, name: "Jane" },
-			{ age: 30, name: "John" },
-			{ age: 18, name: "Bob" },
-			{ age: 18, name: "Andrew" },
-		]);
-	});
+		it("should sort records by string", (context) => {
+			assert.equal(sortByDesc(context.dummies, "age"), [
+				{ age: 40, name: "Jane" },
+				{ age: 30, name: "John" },
+				{ age: 18, name: "Andrew" },
+				{ age: 18, name: "Bob" },
+			]);
+		});
 
-	it("should sort records by string", () => {
-		assert.equal(sortByDesc(dummies, "age"), [
-			{ age: 40, name: "Jane" },
-			{ age: 30, name: "John" },
-			{ age: 18, name: "Andrew" },
-			{ age: 18, name: "Bob" },
-		]);
-	});
-
-	it("should sort records by array", () => {
-		assert.equal(sortByDesc(dummies, ["age"]), [
-			{ age: 40, name: "Jane" },
-			{ age: 30, name: "John" },
-			{ age: 18, name: "Andrew" },
-			{ age: 18, name: "Bob" },
-		]);
-	});
-});
+		it("should sort records by array", (context) => {
+			assert.equal(sortByDesc(context.dummies, ["age"]), [
+				{ age: 40, name: "Jane" },
+				{ age: 30, name: "John" },
+				{ age: 18, name: "Andrew" },
+				{ age: 18, name: "Bob" },
+			]);
+		});
+	},
+);
--- packages/helpers/source/immutable/sort-by.test.js
+++ packages/helpers/source/immutable/sort-by.test.js
@@ -1,39 +1,41 @@
-import { describe } from "@payvo/sdk-test";
+import { describeWithContext } from "@payvo/sdk-test";
 
 import { sortBy } from "./sort-by";
 
-const dummies = [
-	{ age: 30, name: "John" },
-	{ age: 40, name: "Jane" },
-	{ age: 18, name: "Andrew" },
-	{ age: 18, name: "Bob" },
-];
+describeWithContext(
+	"sortBy",
+	() => ({ dummies: [
+		{ age: 30, name: "John" },
+		{ age: 40, name: "Jane" },
+		{ age: 18, name: "Andrew" },
+		{ age: 18, name: "Bob" },
+	]}),
+	({ assert, it }) => {
+		it("should sort records without iteratees", (context) => {
+			assert.equal(sortBy(context.dummies), [
+				{ age: 30, name: "John" },
+				{ age: 40, name: "Jane" },
+				{ age: 18, name: "Andrew" },
+				{ age: 18, name: "Bob" },
+			]);
+		});
 
-describe("sortBy", async ({ assert, it }) => {
-	it("should sort records without iteratees", () => {
-		assert.equal(sortBy(dummies), [
-			{ age: 30, name: "John" },
-			{ age: 40, name: "Jane" },
-			{ age: 18, name: "Andrew" },
-			{ age: 18, name: "Bob" },
-		]);
-	});
+		it("should sort records by string", (context) => {
+			assert.equal(sortBy(context.dummies, "age"), [
+				{ age: 18, name: "Andrew" },
+				{ age: 18, name: "Bob" },
+				{ age: 30, name: "John" },
+				{ age: 40, name: "Jane" },
+			]);
+		});
 
-	it("should sort records by string", () => {
-		assert.equal(sortBy(dummies, "age"), [
-			{ age: 18, name: "Andrew" },
-			{ age: 18, name: "Bob" },
-			{ age: 30, name: "John" },
-			{ age: 40, name: "Jane" },
-		]);
-	});
-
-	it("should sort records by array", () => {
-		assert.equal(sortBy(dummies, ["age"]), [
-			{ age: 18, name: "Andrew" },
-			{ age: 18, name: "Bob" },
-			{ age: 30, name: "John" },
-			{ age: 40, name: "Jane" },
-		]);
-	});
-});
+		it("should sort records by array", (context) => {
+			assert.equal(sortBy(context.dummies, ["age"]), [
+				{ age: 18, name: "Andrew" },
+				{ age: 18, name: "Bob" },
+				{ age: 30, name: "John" },
+				{ age: 40, name: "Jane" },
+			]);
+		});
+	},
+);
