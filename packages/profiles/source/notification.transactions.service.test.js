7792153fe6cb49934dd48012bee37dab9aa40942
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-11-26 22:02:31 +0100

    wip
    Signed-off-by: Alessandro Mitelli <alessandro.mitelli@gmail.com>

--- packages/profiles/source/notification.transactions.service.test.js
+++ packages/profiles/source/notification.transactions.service.test.js
@@ -1,213 +1,203 @@
-import { assert, describe, Mockery, loader, test } from "@payvo/sdk-test";
 import "reflect-metadata";
-import { nock } from "@payvo/sdk-test";
+
+import { describeWithContext } from "@payvo/sdk-test";
 
 import { identity } from "../test/fixtures/identity";
 import { bootContainer, importByMnemonic } from "../test/mocking";
-import { Profile } from "./profile";
-
 import { NotificationRepository } from "./notification.repository";
+import { INotificationTypes } from "./notification.repository.contract";
 import { ProfileTransactionNotificationService } from "./notification.transactions.service";
-import { INotificationTypes, IProfileTransactionNotificationService } from "./notification.repository.contract";
-import { IProfile } from "./profile.contract";
-import { ExtendedConfirmedTransactionData } from "./transaction.dto";
+import { Profile } from "./profile";
 import { ProfileSetting } from "./profile.enum.contract";
-const NotificationTransactionFixtures = require("../test/fixtures/client/notification-transactions.json");
+import { ExtendedConfirmedTransactionData } from "./transaction.dto";
 
-const includedTransactionNotificationId = NotificationTransactionFixtures.data[1].id;
-const excludedTransactionNotificationId = NotificationTransactionFixtures.data[3].id;
-
-let notificationsRepository;
-let subjectTransactionNotificationService;
-let profile;
-
-describe("ProfileTransactionNotificationService", ({ afterEach, beforeEach, test }) => {
-	test.before(async () => {
-		bootContainer();
+describeWithContext("ProfileTransactionNotificationService", { mnemonic: identity.mnemonic }, ({ beforeEach, it, assert, loader, nock, stub }) => {
+	beforeEach(async (context) => {
+		bootContainer({ flush: true });
 
 		nock.fake(/.+/)
 			.get("/api/node/configuration/crypto")
-			.reply(200, require("../test/fixtures/client/cryptoConfiguration.json"))
+			.reply(200, loader.json("test/fixtures/client/cryptoConfiguration.json"))
 			.get("/api/node/configuration")
-			.reply(200, require("../test/fixtures/client/configuration.json"))
+			.reply(200, loader.json("test/fixtures/client/configuration.json"))
 			.get("/api/peers")
-			.reply(200, require("../test/fixtures/client/peers.json"))
+			.reply(200, loader.json("test/fixtures/client/peers.json"))
 			.get("/api/node/syncing")
-			.reply(200, require("../test/fixtures/client/syncing.json"))
+			.reply(200, loader.json("test/fixtures/client/syncing.json"))
 			.get("/api/wallets/D6i8P5N44rFto6M6RALyUXLLs7Q1A1WREW")
-			.reply(200, require("../test/fixtures/client/wallet.json"))
+			.reply(200, loader.json("test/fixtures/client/wallet.json"))
 			.persist();
+
+		context.profile = new Profile({ avatar: "avatar", data: "", id: "uuid", name: "name" });
+		await importByMnemonic(context.profile, context.mnemonic, "ARK", "ark.devnet");
+
+		context.notificationsRepository = new NotificationRepository(context.profile);
+		context.subject = new ProfileTransactionNotificationService(context.profile, context.notificationsRepository);
+
+		context.notificationTransactionFixtures = loader.json("test/fixtures/client/notification-transactions.json");
+		context.includedTransactionNotificationId = context.notificationTransactionFixtures.data[1].id;
+		context.excludedTransactionNotificationId = context.notificationTransactionFixtures.data[3].id;
 	});
 
-	test.before.each(async () => {
-		profile = new Profile({ id: "uuid", name: "name", avatar: "avatar", data: "" });
-		await importByMnemonic(profile, identity.mnemonic, "ARK", "ark.devnet");
+	it("#recent", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		notificationsRepository = new NotificationRepository(profile);
-		subject = new ProfileTransactionNotificationService(profile, notificationsRepository);
+		await context.subject.sync({ limit: 20 });
+
+		assert.length(context.subject.recent(), 2);
+		assert.length(context.subject.recent(10), 2);
+
+		await context.subject.sync();
+		assert.length(context.subject.recent(1), 1);
 	});
 
-	test("#recent", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#sync", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		await subject.sync({ limit: 20 });
+		context.profile.settings().set(ProfileSetting.UseTestNetworks, true);
 
-		assert.length(subject.recent(), 2);
-		assert.length(subject.recent(10), 2);
+		await context.subject.sync({ limit: 10 });
 
-		await subject.sync();
-		assert.length(subject.recent(1), 1);
+		assert.length(context.subject.transactions(), 2);
+		assert.false(context.subject.has(context.excludedTransactionNotificationId));
+		assert.true(context.subject.has(context.includedTransactionNotificationId));
 	});
 
-	test("#sync", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
-
-		profile.settings().set(ProfileSetting.UseTestNetworks, true);
-
-		await subject.sync({ limit: 10 });
-
-		assert.length(subject.transactions(), 2);
-		assert.false(subject.has(excludedTransactionNotificationId));
-		assert.true(subject.has(includedTransactionNotificationId));
-	});
-
-	test("should return empty responses for test networks", async () => {
+	it("should return empty responses for test networks", async (context) => {
 		nock.fake(/.+/)
 			.get("/api/transactions")
 			.query(true)
-			.reply(200, require("../test/fixtures/client/transactions-empty.json"));
+			.reply(200, loader.json("test/fixtures/client/transactions-empty.json"));
 
-		profile.settings().set(ProfileSetting.UseTestNetworks, false);
+		context.profile.settings().set(ProfileSetting.UseTestNetworks, false);
 
-		await subject.sync();
+		await context.subject.sync();
 
-		assert.length(subject.transactions(), 0);
-		assert.false(subject.has(excludedTransactionNotificationId));
-		assert.false(subject.has(includedTransactionNotificationId));
+		assert.length(context.subject.transactions(), 0);
+		assert.false(context.subject.has(context.excludedTransactionNotificationId));
+		assert.false(context.subject.has(context.includedTransactionNotificationId));
 	});
 
-	test("#has", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#has", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		await subject.sync();
+		await context.subject.sync();
 
-		assert.false(subject.has(excludedTransactionNotificationId));
-		assert.true(subject.has(includedTransactionNotificationId));
+		assert.false(context.subject.has(context.excludedTransactionNotificationId));
+		assert.true(context.subject.has(context.includedTransactionNotificationId));
 	});
 
-	test("#findByTransactionId", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#findByTransactionId", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		await subject.sync();
+		await context.subject.sync();
 
-		assert.truthy(subject.findByTransactionId(includedTransactionNotificationId));
-		assert.undefined(subject.findByTransactionId("unknown"));
+		assert.truthy(context.subject.findByTransactionId(context.includedTransactionNotificationId));
+		assert.undefined(context.subject.findByTransactionId("unknown"));
 	});
 
-	test("#forget", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#forget", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		await subject.sync();
+		await context.subject.sync();
 
-		assert.truthy(subject.findByTransactionId(includedTransactionNotificationId));
+		assert.truthy(context.subject.findByTransactionId(context.includedTransactionNotificationId));
 
-		subject.forget("unknown");
-		subject.forget(includedTransactionNotificationId);
+		context.subject.forget("unknown");
+		context.subject.forget(context.includedTransactionNotificationId);
 
-		assert.undefined(subject.findByTransactionId(includedTransactionNotificationId));
+		assert.undefined(context.subject.findByTransactionId(context.includedTransactionNotificationId));
 	});
 
-	test("#forgetByRecipient", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures).persist();
+	it("#forgetByRecipient", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures).persist();
 
-		await subject.sync({ limit: 20 });
-		const wallet = profile.wallets().first();
-		assert.true(subject.has(includedTransactionNotificationId));
-		assert.length(subject.transactions(), 2);
+		await context.subject.sync({ limit: 20 });
+		const wallet = context.profile.wallets().first();
+		assert.true(context.subject.has(context.includedTransactionNotificationId));
+		assert.length(context.subject.transactions(), 2);
 
-		subject.forgetByRecipient("unknown");
+		context.subject.forgetByRecipient("unknown");
 
-		assert.true(subject.has(includedTransactionNotificationId));
+		assert.true(context.subject.has(context.includedTransactionNotificationId));
 
-		subject.forgetByRecipient(wallet.address());
+		context.subject.forgetByRecipient(wallet.address());
 
-		assert.false(subject.has(includedTransactionNotificationId));
-		assert.length(subject.transactions(), 0);
+		assert.false(context.subject.has(context.includedTransactionNotificationId));
+		assert.length(context.subject.transactions(), 0);
 	});
 
-	test("#markAsRead", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#markAsRead", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		await subject.sync({ limit: 20 });
+		await context.subject.sync({ limit: 20 });
 
-		const notification = subject.findByTransactionId(includedTransactionNotificationId);
+		const notification = context.subject.findByTransactionId(context.includedTransactionNotificationId);
 		assert.undefined(notification?.read_at);
 
-		subject.markAsRead("unknown");
-		subject.markAsRead(includedTransactionNotificationId);
+		context.subject.markAsRead("unknown");
+		context.subject.markAsRead(context.includedTransactionNotificationId);
 
-		assert.truthy(subject.findByTransactionId(includedTransactionNotificationId)?.read_at);
+		assert.truthy(context.subject.findByTransactionId(context.includedTransactionNotificationId)?.read_at);
 	});
 
-	test("#transactions", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#transactions", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		await subject.sync({ limit: 20 });
+		await context.subject.sync({ limit: 20 });
 
-		assert.length(subject.transactions(), 2);
-		assert.length(subject.transactions(1), 1);
+		assert.length(context.subject.transactions(), 2);
+		assert.length(context.subject.transactions(1), 1);
 	});
 
-	test("#transactions", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#transactions", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		await subject.sync({ limit: 20 });
+		await context.subject.sync({ limit: 20 });
 
-		assert.instance(subject.transaction(includedTransactionNotificationId), ExtendedConfirmedTransactionData);
+		assert.instance(context.subject.transaction(context.includedTransactionNotificationId), ExtendedConfirmedTransactionData);
 	});
 
-	test("should handle undefined timestamp", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("should handle undefined timestamp", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		const transactions = await profile.transactionAggregate().received({ limit: 10 });
-		const transaction = transactions.findById(NotificationTransactionFixtures.data[2].id);
+		const transactions = await context.profile.transactionAggregate().received({ limit: 10 });
+		const transaction = transactions.findById(context.notificationTransactionFixtures.data[2].id);
 
-		Mockery.stub(transaction, "timestamp").returnValue(undefined);
-		Mockery.stub(profile.transactionAggregate(), "received").resolvedValue(transactions);
+		stub(transaction, "timestamp").returnValue();
+		stub(context.profile.transactionAggregate(), "received").resolvedValue(transactions);
 
-		await subject.sync();
-		assert.length(subject.recent(10), 2);
-		assert.length(subject.recent(), 2);
-		assert.length(subject.transactions(), 2);
+		await context.subject.sync();
+		assert.length(context.subject.recent(10), 2);
+		assert.length(context.subject.recent(), 2);
+		assert.length(context.subject.transactions(), 2);
 
 		// jest.restoreAllMocks();
 
-		await subject.sync();
-		assert.length(subject.recent(10), 2);
-		assert.length(subject.recent(), 2);
+		await context.subject.sync();
+		assert.length(context.subject.recent(10), 2);
+		assert.length(context.subject.recent(), 2);
 	});
 
-	test("#markAllAsRead", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#markAllAsRead", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		await subject.sync({ limit: 20 });
-		notificationsRepository.push({
-			type: INotificationTypes.Release,
+		await context.subject.sync({ limit: 20 });
+		context.notificationsRepository.push({
 			meta: { version: "3.0.0" },
+			type: INotificationTypes.Release,
 		});
 
-		assert.length(notificationsRepository.unread(), 3);
-		assert.length(subject.recent(), 2);
-		subject.markAllAsRead();
-		assert.length(notificationsRepository.unread(), 1);
+		assert.length(context.notificationsRepository.unread(), 3);
+		assert.length(context.subject.recent(), 2);
+		context.subject.markAllAsRead();
+		assert.length(context.notificationsRepository.unread(), 1);
 	});
 
-	test("#isSyncing", async () => {
-		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, NotificationTransactionFixtures);
+	it("#isSyncing", async (context) => {
+		nock.fake(/.+/).get("/api/transactions").query(true).reply(200, context.notificationTransactionFixtures);
 
-		assert.false(subject.isSyncing());
-		await subject.sync({ limit: 20 });
+		assert.false(context.subject.isSyncing());
+		await context.subject.sync({ limit: 20 });
 	});
 });
-
-test.run();
