fbcaa079091665eb56baff87781d3895e3dc90aa
goga-m <goga.mamoulasvili@gmail.com>
2021-11-26 15:32:28 +0200

    wip

--- packages/avax/source/private-key.service.test.js
+++ packages/avax/source/private-key.service.test.js
@@ -3,15 +3,13 @@ import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { PrivateKeyService } from "./private-key.service";
 
-let subject;
-
 describe("PrivateKeyService", async ({ assert, beforeEach, it }) => {
-	beforeEach(async () => {
-		subject = await createService(PrivateKeyService);
+	beforeEach(async (context) => {
+		context.subject = await createService(PrivateKeyService);
 	});
 
-	it("should generate an output from a mnemonic", async () => {
-		assert.equal(await subject.fromMnemonic(identity.mnemonic), {
+	it("should generate an output from a mnemonic", async (context) => {
+		assert.equal(await context.subject.fromMnemonic(identity.mnemonic), {
 			path: "m/44'/9000'/0'/0/0",
 			privateKey: "rC7DsPL1zKuPnwnqHSnShdXxeMReKWLBJgKcuJ1ZLUCUrzRni",
 		});
--- packages/avax/source/public-key.service.test.js
+++ packages/avax/source/public-key.service.test.js
@@ -3,15 +3,13 @@ import { identity } from "../test/fixtures/identity";
 import { createService } from "../test/mocking";
 import { PublicKeyService } from "./public-key.service";
 
-let subject;
-
 describe("PublicKeyService", async ({ assert, beforeEach, it }) => {
-	beforeEach(async () => {
-		subject = await createService(PublicKeyService);
+	beforeEach(async (context) => {
+		context.subject = await createService(PublicKeyService);
 	});
 
-	it("should generate an output from a mnemonic", async () => {
-		assert.equal(await subject.fromMnemonic(identity.mnemonic), {
+	it("should generate an output from a mnemonic", async (context) => {
+		assert.equal(await context.subject.fromMnemonic(identity.mnemonic), {
 			path: "m/44'/9000'/0'/0/0",
 			publicKey: "7qobgTQPiy3mH4tvjabDjapPVrh9Tnkb3tpn2yY37hsEyxaSjW",
 		});
