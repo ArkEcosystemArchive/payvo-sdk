8f57264ca5de3d4f991becc3995d68a75c89bfa2
faustbrian <faustbrian@users.noreply.github.com>
2021-11-27 10:40:34 +0000

    style: resolve style guide violations

--- packages/test/source/describe.ts
+++ packages/test/source/describe.ts
@@ -12,40 +12,40 @@ import { nock } from "./nock.js";
 type ContextFunction = () => Context;
 
 const runSuite = (suite: Test, callback: Function, dataset?: unknown): void => {
-    suite.before(() => nock.disableNetConnect());
+	suite.before(() => nock.disableNetConnect());
 
-    suite.after(() => nock.enableNetConnect());
-    suite.after.each(() => nock.cleanAll());
+	suite.after(() => nock.enableNetConnect());
+	suite.after.each(() => nock.cleanAll());
 
-    callback({
-        afterAll: async (callback_: Function) => suite.after(runHook(callback_)),
-        afterEach: async (callback_: Function) => suite.after.each(runHook(callback_)),
-        assert,
-        beforeAll: async (callback_: Function) => suite.before(runHook(callback_)),
-        beforeEach: async (callback_: Function) => suite.before.each(runHook(callback_)),
-        dataset,
-        each: each(suite),
-        it: suite,
-        loader,
-        nock,
-        only: suite.only,
-        schema,
-        skip: suite.skip,
-        spy,
-        stub: (target: object, method: string) => new Mockery(target, method),
-        test: suite,
-    });
+	callback({
+		afterAll: async (callback_: Function) => suite.after(runHook(callback_)),
+		afterEach: async (callback_: Function) => suite.after.each(runHook(callback_)),
+		assert,
+		beforeAll: async (callback_: Function) => suite.before(runHook(callback_)),
+		beforeEach: async (callback_: Function) => suite.before.each(runHook(callback_)),
+		dataset,
+		each: each(suite),
+		it: suite,
+		loader,
+		nock,
+		only: suite.only,
+		schema,
+		skip: suite.skip,
+		spy,
+		stub: (target: object, method: string) => new Mockery(target, method),
+		test: suite,
+	});
 
-    suite.run();
+	suite.run();
 };
 
 export const describe = (title: string, callback: Function): void => runSuite(suite(title), callback);
 
 export const describeWithContext = (title: string, context: Context | ContextFunction, callback: Function): void =>
-    runSuite(suite(title, typeof context === "function" ? context() : context), callback);
+	runSuite(suite(title, typeof context === "function" ? context() : context), callback);
 
 export const describeEach = (title: string, callback: Function, datasets: unknown[]): void => {
-    for (const dataset of datasets) {
-        runSuite(suite(formatName(title, dataset)), callback);
-    }
+	for (const dataset of datasets) {
+		runSuite(suite(formatName(title, dataset)), callback);
+	}
 };
