aa84e441d5103b106c82186a58b4a3f015026e3a
Edgar Goetzendorff <hello@dated.fun>
2021-11-29 16:19:35 +0100

    chore(profiles): expose `ExchangeTransaction` and `ReadOnlyWallet` (#569)

--- packages/profiles/source/index.ts
+++ packages/profiles/source/index.ts
@@ -3,7 +3,9 @@ import "reflect-metadata";
 export * from "./container.models";
 export * from "./env.models";
 export * from "./env";
+export * from "./exchange-transaction";
 export * from "./profile";
+export * from "./read-only-wallet";
 export * from "./wallet";
 
 export * as Contracts from "./contracts";
