9ad34d66a8fb05d12eb7db73def4600d82c26018
Brian Faust <hello@basecode.sh>
2021-11-30 04:15:39 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-pnpm run build
+pnpm run clean
+pnpm run rebuild
+NODE_ENV=production pnpm run build:release
 
 for dir in `find packages -mindepth 1 -maxdepth 1 -type d | sort -nr`; do
     cd $dir
     echo $PWD
-    NPM_AUTH_TOKEN=$1 pnpm publish --access=public
+    NPM_AUTH_TOKEN=$1 pnpm publish --access=public --publish-branch=master
     cd ../..
 done
