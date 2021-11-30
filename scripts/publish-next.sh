594ee7748ab650bfee1d960ee9ac3e37ee34045d
Brian Faust <hello@basecode.sh>
2021-11-30 03:56:48 +0200

    release: 4.0.0-next.9

---
+++ scripts/publish-next.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pnpm run build
+NODE_ENV=production pnpm run build:release
+
+for dir in `find packages -mindepth 1 -maxdepth 1 -type d | sort -nr`; do
+    cd $dir
+    echo $PWD
+    NPM_AUTH_TOKEN=$1 pnpm publish --access=public --publish-branch=testbranch --tag=next
+    cd ../..
+done
