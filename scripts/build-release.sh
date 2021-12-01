8359a5bcf729edb3fd002974ec1ed8dd1db15927
goga-m <goga.mamoulasvili@gmail.com>
2021-12-01 16:31:43 +0200

    wip

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -15,24 +15,29 @@ bundle http-fetch
 bundle markets
 
 # 2. Coins
-bundle ada
-bundle ark
-bundle atom
-bundle avax
-bundle btc
-bundle dot
-bundle egld
-bundle eos
-bundle eth
-bundle lsk
-bundle luna
-bundle nano
-bundle neo
-bundle sol
-bundle trx
-bundle xlm
-bundle xrp
-bundle zil
+if [[ $1 != '' ]]
+then
+	bundle $1
+else
+	bundle ada
+	bundle ark
+	bundle atom
+	bundle avax
+	bundle btc
+	bundle dot
+	bundle egld
+	bundle eos
+	bundle eth
+	bundle lsk
+	bundle luna
+	bundle nano
+	bundle neo
+	bundle sol
+	bundle trx
+	bundle xlm
+	bundle xrp
+	bundle zil
+fi
 
 # 3. Integration
 bundle profiles
