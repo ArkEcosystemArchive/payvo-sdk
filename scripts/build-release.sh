9fe6d8e0e095dfa705d52fa48886d08bcc74a12e
Brian Faust <hello@basecode.sh>
2021-12-01 11:10:39 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,34 +1,38 @@
-cd packages
+function bundle() {
+	cd packages/$1
+	pnpm run build:release
+	cd ../..
+}
 
 # 1. Prerequisites
-cd test && pnpm run build:release && cd ..
-cd helpers && pnpm run build:release && cd ..
-cd intl && pnpm run build:release && cd ..
-cd cryptography && pnpm run build:release && cd ..
-cd sdk && pnpm run build:release && cd ..
-cd news && pnpm run build:release && cd ..
-cd http-fetch && pnpm run build:release && cd ..
-cd markets && pnpm run build:release && cd ..
+bundle test
+bundle helpers
+bundle intl
+bundle cryptography
+bundle sdk
+bundle news
+bundle http-fetch
+bundle markets
 
 # 2. Coins
-cd ada && pnpm run build:release && cd ..
-cd ark && pnpm run build:release && cd ..
-cd atom && pnpm run build:release && cd ..
-cd avax && pnpm run build:release && cd ..
-cd btc && pnpm run build:release && cd ..
-cd dot && pnpm run build:release && cd ..
-cd egld && pnpm run build:release && cd ..
-cd eos && pnpm run build:release && cd ..
-cd eth && pnpm run build:release && cd ..
-cd lsk && pnpm run build:release && cd ..
-cd luna && pnpm run build:release && cd ..
-cd nano && pnpm run build:release && cd ..
-cd neo && pnpm run build:release && cd ..
-cd sol && pnpm run build:release && cd ..
-cd trx && pnpm run build:release && cd ..
-cd xlm && pnpm run build:release && cd ..
-cd xrp && pnpm run build:release && cd ..
-cd zil && pnpm run build:release && cd ..
+bundle ada
+bundle ark
+bundle atom
+bundle avax
+bundle btc
+bundle dot
+bundle egld
+bundle eos
+bundle eth
+bundle lsk
+bundle luna
+bundle nano
+bundle neo
+bundle sol
+bundle trx
+bundle xlm
+bundle xrp
+bundle zil
 
 # 3. Integration
-cd profiles && pnpm run build:release && cd ..
+bundle profiles
