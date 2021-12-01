6e0b218bd416f49e5db15de4a01d2a66ab4ad7e7
w3ea <w3ea@users.noreply.github.com>
2021-12-01 14:54:37 +0000

    style: resolve style guide violations

--- packages/lsk/source/networks/shared.ts
+++ packages/lsk/source/networks/shared.ts
@@ -37,7 +37,7 @@ export const featureFlags: Networks.NetworkManifestFeatureFlags = {
 	Message: ["sign", "verify"],
 	PrivateKey: ["mnemonic.bip39"],
 	PublicKey: ["mnemonic.bip39"],
-	Transaction: ["delegateRegistration", "transfer", "unlockToken", "vote"],
+	Transaction: ["delegateRegistration", "multiSignature", "transfer", "unlockToken", "vote"],
 };
 
 export const explorer = {
