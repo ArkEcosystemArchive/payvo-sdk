d37d8d5321c03868c472ea46300a614ce790faad
Brian Faust <22145591+faustbrian@users.noreply.github.com>
2021-12-02 13:46:03 +0200

    refactor(cryptography): use `secp256k1` for non-schnorr functions (#601)

--- packages/cryptography/source/secp256k1.ts
+++ packages/cryptography/source/secp256k1.ts
@@ -1,24 +1,29 @@
 import { secp256k1 as bcrypto } from "bcrypto";
+import * as secp from "secp256k1";
 
 class Secp256k1 {
 	public publicKeyCreate(privateKey: Buffer, compressed: boolean): Buffer {
-		return bcrypto.publicKeyCreate(privateKey, compressed);
+		return Buffer.from(secp.publicKeyCreate(privateKey, compressed));
 	}
 
 	public publicKeyVerify(publicKey: Buffer): boolean {
-		return bcrypto.publicKeyVerify(publicKey);
+		try {
+			return secp.publicKeyVerify(publicKey);
+		} catch {
+			return false;
+		}
 	}
 
 	public publicKeyCombine(publicKeys: Buffer[]): Buffer {
-		return bcrypto.publicKeyCombine(publicKeys);
+		return Buffer.from(secp.publicKeyCombine(publicKeys));
 	}
 
 	public sign(hash: Buffer, privateKey: Buffer): Buffer {
-		return bcrypto.sign(hash, privateKey);
+		return Buffer.from(secp.ecdsaSign(hash, privateKey).signature);
 	}
 
 	public verify(hash: Buffer, signature: Buffer, publicKey: Buffer): boolean {
-		return bcrypto.verify(hash, signature, publicKey);
+		return secp.ecdsaVerify(signature, hash, publicKey);
 	}
 
 	public schnorrSign(hash: Buffer, privateKey: Buffer): Buffer {
