5846bdc7c7b225c280b6937674714ec7bc0fce02
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-12-03 11:17:12 +0100

    wip

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,6 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const ResolveTypeScriptPlugin = require("resolve-typescript-plugin").default;
 
 const common = {
-	...require("./webpack.base"),
 	target: ["web", "es2022"],
 	devtool: "source-map",
 	experiments: {
@@ -13,6 +12,29 @@ const common = {
 	plugins: [
 		new NodePolyfillPlugin(),
 	],
+	mode: "production",
+	entry: require("path").resolve(process.cwd(), "source/index.ts"),
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	optimization: {
+		minimize: process.env.NODE_ENV === "production",
+		sideEffects: false,
+	},
+	performance: {
+		hints: "warning",
+		maxAssetSize: 10485760,
+		maxEntrypointSize: 10485760,
+	},
+	stats: {
+		errorDetails: true,
+	},
 	resolve: {
 		extensions: [".tsx", ".ts", ".js"],
 		plugins: [new ResolveTypeScriptPlugin()],
