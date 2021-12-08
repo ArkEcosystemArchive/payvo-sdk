783ca1c98b5e2c47781a913c40c3602963023be6
Brian Faust <hello@basecode.sh>
2021-12-08 05:23:47 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/cryptography/source/uuid.test.ts
+++ packages/cryptography/source/uuid.test.ts
@@ -2,27 +2,9 @@ import { describe } from "@payvo/sdk-test";
 
 import { UUID } from "./uuid";
 
-describe("UUID", ({ assert, it, nock, loader }) => {
-	it("should create a UUID based on the TIMESTAMP method", () => {
-		assert.string(UUID.timestamp());
-	});
-
-	it("should create a UUID based on the MD5 method", () => {
-		assert.is(
-			UUID.md5("Hello World", "1b671a64-40d5-491e-99b0-da01ff1f3341"),
-			"d954df73-1ea5-303b-a2cc-b24265839eec",
-		);
-	});
-
+describe("UUID", ({ assert, it }) => {
 	it("should create a UUID based on the RANDOM method", () => {
 		assert.string(UUID.random());
-	});
-
-	it("should create a UUID based on the SHA1 method", () => {
-		assert.is(
-			UUID.sha1("Hello World", "1b671a64-40d5-491e-99b0-da01ff1f3341"),
-			"a572fa0f-9bfa-5103-9882-16394770ad11",
-		);
 	});
 
 	it("should parse the given value", () => {
