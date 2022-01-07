02d2ef423d36578ab60b34551e41a096c7d0f702
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2022-01-07 12:33:30 +0100

    wip
    Signed-off-by: Alessandro Mitelli <alessandro.mitelli@gmail.com>

--- packages/helpers/source/order-by.ts
+++ packages/helpers/source/order-by.ts
@@ -1,9 +1,9 @@
-import { sort } from "fast-sort";
-
-import { FunctionReturning, Iteratee } from "./types.js";
 import { isFunction } from "./is-function.js";
 import { isString } from "./is-string.js";
 import { map } from "./map.js";
+import { FunctionReturning, Iteratee } from "./types.js";
+
+const fastSort = await import ("fast-sort").then(module => module.sort);
 
 export const orderBy = <T>(values: T[], iteratees: Iteratee | Iteratee[], orders: string | string[]): T[] => {
 	if (isString(iteratees)) {
@@ -16,7 +16,7 @@ export const orderBy = <T>(values: T[], iteratees: Iteratee | Iteratee[], orders
 		orders = [orders];
 	}
 
-	return sort(values).by(
+	return fastSort(values).by(
 		map(iteratees as any, (_: string, index: number) => ({ [orders[index]]: iteratees[index] })),
 	);
 };
--- packages/helpers/source/sort-by-desc.ts
+++ packages/helpers/source/sort-by-desc.ts
@@ -1,6 +1,8 @@
-import { sort, ISortBy, ISortByFunction } from "fast-sort";
+import { ISortBy, ISortByFunction } from "fast-sort";
+
+const fastSort = await import ("fast-sort").then(module => module.sort);
 
 export const sortByDesc = <T>(
 	values: T[],
 	iteratees?: ISortByFunction<T> | keyof T | (ISortByFunction<T> | keyof T)[] | ISortBy<T>[] | undefined,
-): T[] => sort(values).desc(iteratees);
+): T[] => fastSort(values).desc(iteratees);
