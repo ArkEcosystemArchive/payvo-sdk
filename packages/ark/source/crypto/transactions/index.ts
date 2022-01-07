da1f98e87fe885e750cfb108e9bc543610c0a5ea
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2022-01-07 11:50:48 +0100

    wip
    Signed-off-by: Alessandro Mitelli <alessandro.mitelli@gmail.com>

--- packages/ark/source/crypto/transactions/index.ts
+++ packages/ark/source/crypto/transactions/index.ts
@@ -1,9 +1,9 @@
-export * from "./builders";
+export * from "./builders/index.js";
 export * from "./deserializer.js";
 export * from "./factory.js";
 export { TransactionConstructor, transactionRegistry as TransactionRegistry } from "./registry.js";
 export * from "./serializer.js";
-export * from "./signer";
-export * from "./types";
+export * from "./signer.js";
+export * from "./types/index.js";
 export * from "./utils.js";
 export * from "./verifier.js";
