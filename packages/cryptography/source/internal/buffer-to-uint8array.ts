af779d4cb4100275e8a97b9a5a56a06f5175cc90
Brian Faust <hello@basecode.sh>
2022-01-12 06:13:46 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ packages/cryptography/source/internal/buffer-to-uint8array.ts
@@ -0,0 +1,12 @@
+export const toArrayBuffer = (value: Buffer): Uint8Array => {
+	const buffer = new ArrayBuffer(value.length);
+	const result = new Uint8Array(buffer);
+
+	for (const [index, element] of value.entries()) {
+		result[index] = element;
+	}
+
+	return result;
+}
+
+export const toArrayBufferList = (values: Buffer[]): Uint8Array[] => values.map((value: Buffer) => toArrayBuffer(value));
--- packages/cryptography/source/secp256k1.ts
+++ packages/cryptography/source/secp256k1.ts
@@ -1,28 +1,30 @@
-import * as secp from "secp256k1";
+import { ecdsaSign, ecdsaVerify,publicKeyCombine, publicKeyCreate, publicKeyVerify } from "secp256k1";
+
+import { toArrayBuffer, toArrayBufferList } from "./internal/buffer-to-uint8array.js";
 
 class Secp256k1 {
 	public publicKeyCreate(privateKey: Buffer, compressed: boolean): Buffer {
-		return Buffer.from(secp.publicKeyCreate(privateKey, compressed));
+		return Buffer.from(publicKeyCreate(toArrayBuffer(privateKey), toArrayBuffer(compressed)));
 	}
 
 	public publicKeyVerify(publicKey: Buffer): boolean {
 		try {
-			return secp.publicKeyVerify(publicKey);
+			return publicKeyVerify(toArrayBuffer(publicKey));
 		} catch {
 			return false;
 		}
 	}
 
 	public publicKeyCombine(publicKeys: Buffer[]): Buffer {
-		return Buffer.from(secp.publicKeyCombine(publicKeys));
+		return Buffer.from(publicKeyCombine(toArrayBufferList(publicKeys)));
 	}
 
 	public sign(hash: Buffer, privateKey: Buffer): Buffer {
-		return Buffer.from(secp.ecdsaSign(hash, privateKey).signature);
+		return Buffer.from(ecdsaSign(toArrayBuffer(hash), toArrayBuffer(privateKey)).signature);
 	}
 
 	public verify(hash: Buffer, signature: Buffer, publicKey: Buffer): boolean {
-		return secp.ecdsaVerify(signature, hash, publicKey);
+		return ecdsaVerify(toArrayBuffer(signature), toArrayBuffer(hash), toArrayBuffer(publicKey));
 	}
 }
 
