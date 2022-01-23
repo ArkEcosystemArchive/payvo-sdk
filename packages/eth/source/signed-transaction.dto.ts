dc4d6567243cb786d058c981ddbd21f744344e6f
Josip Crnković <jcrnkovic95@gmail.com>
2022-01-23 16:04:04 +0100

    implement basic methods to the signed transaction DTO

--- packages/eth/source/signed-transaction.dto.ts
+++ packages/eth/source/signed-transaction.dto.ts
@@ -1,4 +1,4 @@
-import { Contracts, DTO, Exceptions, IoC } from "@payvo/sdk";
+import { Contracts, DTO } from "@payvo/sdk";
 import { DateTime } from "@payvo/sdk-intl";
 import { BigNumber } from "@payvo/sdk-helpers";
 
@@ -7,22 +7,22 @@ export class SignedTransactionData
 	implements Contracts.SignedTransactionData
 {
 	public override sender(): string {
-		throw new Exceptions.NotImplemented(this.constructor.name, this.sender.name);
+		return this.signedData.from;
 	}
 
 	public override recipient(): string {
-		throw new Exceptions.NotImplemented(this.constructor.name, this.recipient.name);
+		return this.signedData.to;
 	}
 
 	public override amount(): BigNumber {
-		throw new Exceptions.NotImplemented(this.constructor.name, this.amount.name);
+		return this.bigNumberService.make(this.signedData.amount);
 	}
 
 	public override fee(): BigNumber {
-		throw new Exceptions.NotImplemented(this.constructor.name, this.fee.name);
+		return this.bigNumberService.make(this.signedData.fee);
 	}
 
 	public override timestamp(): DateTime {
-		throw new Exceptions.NotImplemented(this.constructor.name, this.timestamp.name);
+		return DateTime.make(this.signedData.timestamp)
 	}
 }
