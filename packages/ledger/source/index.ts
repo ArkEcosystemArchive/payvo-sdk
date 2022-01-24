815287b65c43116684b32d8e887a9e4bc57136b1
goga-m <goga.mamoulasvili@gmail.com>
2022-01-24 17:14:52 +0200

    wip

--- packages/ledger/source/index.ts
+++ packages/ledger/source/index.ts
@@ -1,52 +1,31 @@
 // Based on https://github.com/near/near-ledger-js/blob/master/supportedTransports.js
-
 import LedgerU2F from "@ledgerhq/hw-transport-u2f";
 import LedgerHID from "@ledgerhq/hw-transport-webhid";
 import LedgerUSB from "@ledgerhq/hw-transport-webusb";
-import { Services } from "@payvo/sdk";
 import platform from "platform";
 
-export class LedgerTransportFactory {
-	public static async create(): Promise<Services.LedgerTransport> {
-		return new LedgerTransportFactory().#make();
-	}
 
-	async #make(): Promise<Services.LedgerTransport> {
+export class LedgerTransportFactory {
+	public async supportedTransport(): Promise<LedgerHID | LedgerUSB | LedgerU2F>  {
 		const [supportsHID, supportsUSB, supportsU2F] = await Promise.all([
 			this.#supportsHID(),
 			this.#supportsUSB(),
 			this.#supportsU2F(),
 		]);
 
-		if (!supportsHID && !supportsUSB && !supportsU2F) {
-			throw new Error("No transports appear to be supported.");
-		}
-
-		const supportedTransports: Services.LedgerTransport[] = [];
-
 		if (supportsHID) {
-			supportedTransports.push(LedgerHID);
+			return LedgerHID;
 		}
 
 		if (supportsUSB) {
-			supportedTransports.push(LedgerUSB);
+			return LedgerUSB;
 		}
 
 		if (supportsU2F) {
-			supportedTransports.push(LedgerU2F);
+			return LedgerU2F;
 		}
 
-		for (const transport of supportedTransports) {
-			try {
-				return await transport.create();
-			} catch (error) {
-				if (error.name === "TransportOpenUserCancelled") {
-					throw error;
-				}
-			}
-		}
-
-		throw new Error("Failed to connect through any transport.");
+		throw new Error("No transports appear to be supported.");
 	}
 
 	async #supportsHID(): Promise<boolean> {
@@ -60,7 +39,7 @@ export class LedgerTransportFactory {
 	async #supportsUSB(): Promise<boolean> {
 		try {
 			if (await LedgerUSB.isSupported()) {
-				return platform.os.family !== "Windows" && platform.name !== "Opera";
+				return platform.os?.family !== "Windows" && platform.name !== "Opera";
 			}
 
 			return false;
