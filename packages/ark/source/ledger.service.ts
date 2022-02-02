447d49b95deb01f6ee4d5772c3b0164b73155679
alfonsobries <alfonsobries@gmail.com>
2022-02-01 20:51:45 -0600

    Refactor mapping to handle find wallets

--- packages/ark/source/ledger.service.ts
+++ packages/ark/source/ledger.service.ts
@@ -1,6 +1,8 @@
 import { ARKTransport } from "@arkecosystem/ledger-transport";
 import { Contracts, IoC, Services } from "@payvo/sdk";
+import { LedgerWalletList } from "@payvo/sdk/distribution/esm/ledger.contract";
 import { BIP44, HDKey } from "@payvo/sdk-cryptography";
+
 import { chunk, createRange, formatLedgerDerivationPath } from "./ledger.service.helpers.js";
 
 export class LedgerService extends Services.AbstractLedgerService {
@@ -54,16 +56,18 @@ export class LedgerService extends Services.AbstractLedgerService {
 	public override async scan(options?: {
 		useLegacy: boolean;
 		startPath?: string;
-		importedWalletHandler?: ((wallet: Contracts.WalletData) => void);
+		onProgress?: ((wallet: Contracts.WalletData) => void);
 	}): Promise<Services.LedgerWalletList> {
+
 		const pageSize = 5;
 		let page = 0;
 		const slip44 = this.configRepository.get<number>("network.constants.slip44");
 
 		const addressCache: Record<string, { address: string; publicKey: string }> = {};
-		let wallets: Contracts.WalletData[] = [];
+		let wallets: LedgerWalletList = {};
 
 		let hasMore = true;
+
 		do {
 			const addresses: string[] = [];
 
@@ -73,7 +77,7 @@ export class LedgerService extends Services.AbstractLedgerService {
 			 */
 			if (options?.useLegacy) {
 				for (const accountIndex of createRange(page, pageSize)) {
-					const path: string = formatLedgerDerivationPath({ coinType: slip44, account: accountIndex });
+					const path: string = formatLedgerDerivationPath({ account: accountIndex, coinType: slip44 });
 					const publicKey: string = await this.getPublicKey(path);
 					const { address } = await this.#addressService.fromPublicKey(publicKey);
 
@@ -86,11 +90,18 @@ export class LedgerService extends Services.AbstractLedgerService {
 					identifiers: [...addresses].map((address: string) => ({ type: "address", value: address })),
 				});
 
-				if (options?.importedWalletHandler !== undefined) {
-					collection.items().forEach((wallet: Contracts.WalletData) => options.importedWalletHandler!(wallet));
+				const ledgerWallets = this.mapPathsToWallets(addressCache, collection.items());
+
+				if (options?.onProgress !== undefined) {
+					for (const path in ledgerWallets) {
+						options.onProgress(ledgerWallets[path]);
+					}
 				}
 
-				wallets = wallets.concat(collection.items());
+				wallets = {
+					...wallets,
+					...ledgerWallets
+				};
 
 				hasMore = collection.isNotEmpty();
 			} else {
@@ -130,11 +141,18 @@ export class LedgerService extends Services.AbstractLedgerService {
 				);
 
 				for (const collection of collections) {
-					if (options?.importedWalletHandler !== undefined) {
-						collection.items().forEach((wallet: Contracts.WalletData) => options.importedWalletHandler!(wallet));
+					const ledgerWallets = this.mapPathsToWallets(addressCache, collection.items());
+
+					if (options?.onProgress !== undefined) {
+						for (const path in ledgerWallets) {
+							options.onProgress(ledgerWallets[path]);
+						}
 					}
 
-					wallets = wallets.concat(collection.items());
+					wallets = {
+						...wallets,
+						...ledgerWallets
+					};
 
 					hasMore = collection.isNotEmpty();
 				}
@@ -144,7 +162,7 @@ export class LedgerService extends Services.AbstractLedgerService {
 		} while (hasMore);
 
 		// Return a mapping of paths and wallets that have been found.
-		return this.mapPathsToWallets(addressCache, wallets);
+		return wallets;
 	}
 
 	public override async isNanoS(): Promise<boolean> {
