32542f58f64c7b87808026eed699e31a6bc32aab
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-02-23 01:56:48 +0000

    fix(profiles): multisign transaction payload (#715)

--- packages/profiles/source/wallet-transaction.service.ts
+++ packages/profiles/source/wallet-transaction.service.ts
@@ -292,10 +292,17 @@ export class TransactionService implements ITransactionService {
 		if (this.canBeBroadcasted(id)) {
 			result = await this.#wallet.client().broadcast([transaction.data()]);
 		} else if (transaction.isMultiSignatureRegistration() || transaction.usesMultiSignature()) {
+			const { amount, fee, nonce, ...restOfThePayload } = transaction.data().data();
+
 			result = await this.#wallet
 				.coin()
 				.multiSignature()
-				.broadcast(JSON.parse(JSON.stringify(transaction.data().data(), null, 4)));
+				.broadcast({
+					...JSON.parse(JSON.stringify(restOfThePayload, undefined, 4)),
+					amount: amount.toString(),
+					fee: fee.toString(),
+					nonce: nonce.toString(),
+				});
 		}
 
 		if (result.accepted.includes(transaction.id())) {
